9fe14afb4829f48115b3e376618209fda8ed61ac
Ivan Sieder <ivan@sieder.xyz>
2018-10-02 18:22:09 +0200

    implemented mean

--- src/mean.js
+++ src/mean.js
@@ -1,3 +1,7 @@
-export const mean = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const mean = (numbers = []) => {
+  if (!Array.isArray(numbers) || !numbers.length) {
+    return null;
+  }
+
+  return numbers.reduce((accumulator, number) => accumulator + number, 0) / numbers.length;
 };
