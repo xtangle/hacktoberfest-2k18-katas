8f8c6eb560d4e54e5cf476481e8b17e226ab29c0
Johnny Metz <jcmetz21@gmail.com>
2018-10-02 15:04:56 -0700

    Fix test comparison

--- src/reverseString.test.js
+++ src/reverseString.test.js
@@ -2,10 +2,10 @@ import { reverseString } from './reverseString';
 
 describe('reverseString', () => {
   it('reverse of "hello" is "olleh"', () => {
-    expect(reverseString('hello')).toBeTruthy('olleh')
+    expect(reverseString('hello')).toEqual('olleh')
   });
 
   it('reverse of "i love cheese" is "eseehc evol i"', () => {
-    expect(reverseString('i love cheese')).toBeTruthy('eseehc evol i')
+    expect(reverseString('i love cheese')).toEqual('eseehc evol i')
   })
 })
