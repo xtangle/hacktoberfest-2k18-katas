53ac48819bf3e441bf5cee23f8e47a8b930d867b
Barnabás Sztán-Kovács <31653416+skbarnabas@users.noreply.github.com>
2018-10-02 18:00:48 +0200

    Implementing countEven

--- src/countEven.js
+++ src/countEven.js
@@ -1,3 +1,9 @@
-export const countEven = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const countEven = array => {
+    if (!array)
+        return 0;
+    var evens = 0;
+    for (var i = 0; i < array.length; i++)
+        if (array[i] % 2 === 0)
+            evens++;
+    return evens
 };
