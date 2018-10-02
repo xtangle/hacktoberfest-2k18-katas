d6b1e41c5398a5cefef49f0ac4da58267d337364
KUNAL1612 <kunalchakrabarty@gmail.com>
2018-10-02 21:54:00 +0530

    Implemented countOdd

--- src/countOdd.js
+++ src/countOdd.js
@@ -1,3 +1,22 @@
-export const countOdd = () => {
+export const countOdd = values => {
   // TO IMPLEMENT IN ANOTHER PR
+  if(!values){
+    return 0;
+  }
+  else if(!values.length){
+    return 0;
+  }
+  else{
+    var l=values.length;
+    var i;
+    var count=0;
+    for(i=0;i<l;i++)
+    {
+      if(values[i]%2!=0)
+      {
+        count=count+1;
+      }
+    }
+    return count;
+  }
 };
