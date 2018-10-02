47e5715ca18ce7740beed4ae9f4062deff7fb3ef
benNek <benas@nekrosius.com>
2018-10-02 22:33:18 +0300

    Implement fizzBuzz, add uniq test

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,3 +1,14 @@
-export const fizzBuzz = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const fizzBuzz = (array) => {
+  return array.map(value => {
+    if(value % 5 == 0 && value % 3 == 0) {
+      return 'fizzbuzz';
+    }
+    else if(value % 3 == 0) {
+      return 'fizz';
+    }
+    else if(value % 5 == 0) {
+      return 'buzz';
+    }
+    return value;
+  });
 };
---
+++ src/uniq.js
@@ -0,0 +1,3 @@
+export const uniq = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
