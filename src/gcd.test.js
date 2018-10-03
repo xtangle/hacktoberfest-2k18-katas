a2a6aeeb212bce2f7549c62790ddaf953aa544ef
Renan GEHAN <rgehan94@gmail.com>
2018-10-03 21:23:33 +0200

    Update gcd.test.js

--- src/gcd.test.js
+++ src/gcd.test.js
@@ -2,16 +2,16 @@ import { gcd } from './gcd';
 
 describe('gcd', () => {
   it('does something', () => {
-    expect(gcd(2,8)).toBeTruthy(2); 
+    expect(gcd(2,8)).toEqual(2); 
   });
 
   it('does something else', () => {
-    expect(gcd(5.9)).toBeTruthy(1); 
+    expect(gcd(5.9)).toEqual(1); 
   });
   it('does something else', () => {
-    expect(gcd(1709,1729)).toBeTruthy(1); 
+    expect(gcd(1709,1729)).toEqual(1); 
   });
   it('does something else', () => {
-    expect(gcd(11,121)).toBeTruthy(11); 
+    expect(gcd(11,121)).toEqual(11); 
   });
 })
