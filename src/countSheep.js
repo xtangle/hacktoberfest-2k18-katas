5c6b92ca16cfb7accd7d703ef8b0b4ac788f8afc
lukebrobbs <lukemichaelvocalist@gmail.com>
2018-10-03 12:35:49 +0100

    Added functionality to pow to satisfy tests. Added countSheep function to be implemented. Added unit tests for countSheep

---
+++ src/countSheep.js
@@ -0,0 +1,5 @@
+export const countSheep = arr => {
+  //Given an array as an argument
+  //Should return the number of instances of the workd 'sheep'
+  //Should return 0 if the sirst argument is not an array
+};
