1f469cdb8a8e58da7a8acfcb2c6d7998e48f98a5
chintuyadav <chintuyadav.sr336@gmail.com>
2018-10-03 16:44:52 +0530

    Implemented Factorial.js

--- src/factorial.js
+++ src/factorial.js
@@ -1,3 +1,7 @@
-export const factorial = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const factorial = (num) => {
+// TO IMPLEMENT IN ANOTHER PR
+        if(num==0) {
+                return 1;
+        }
+        return num * factorial(num-1);
 };
---
+++ src/isEmail.test.js
@@ -0,0 +1,11 @@
+import { isEmail } from './isEmail';
+
+describe('isEmail', () => {
+  it('does something', () => {
+    expect(true).toBeTruthy(); // TODO Your test instead
+  });
+
+  it('does something else', () => {
+    expect(true).toBeTruthy(); // TODO Your test instead
+  });
+})
