2707fd8bc514c24a0664be53d40687c6a5b57673
Renan GEHAN <rgehan94@gmail.com>
2018-10-03 13:25:39 +0200

    Update isEmail.test.js

--- src/isEmail.test.js
+++ src/isEmail.test.js
@@ -2,10 +2,10 @@ import { isEmail } from './isEmail';
 
 describe('isEmail', () => {
   it('test@gmail.com is valid email', () => {
-    expect(isEmail("test@gmail.com")).toBeTruthy(); // TODO Your test instead
+    expect(isEmail("test@gmail.com")).toBeTruthy();
   });
 
   it('test@gmailm is not valid email', () => {
-    expect(isEmail("test@gmail")).toBeTruthy(); // TODO Your test instead
+    expect(isEmail("test@gmail")).toBeFalsy();
   });
 })
