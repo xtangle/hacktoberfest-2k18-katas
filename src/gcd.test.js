19113a66b4bc70ba0e5372fc5b8dc5e0e53118cb
lchaglla <lchaglla@maubic.es>
2018-10-03 21:15:33 +0200

    Revert "Fixed tests"
    This reverts commit 7c0cba37ec4d9e429f3e6c1171be9c93dc7ab1d7.

--- src/gcd.test.js
+++ src/gcd.test.js
@@ -2,16 +2,16 @@ import { gcd } from './gcd';
 
 describe('gcd', () => {
   it('does something', () => {
-    expect(gcd(2,8)).toBe(2); 
+    expect(gcd(2,8)).toBeTruthy(2); 
   });
 
   it('does something else', () => {
-    expect(gcd(5,9)).toBe(1); 
+    expect(gcd(5.9)).toBeTruthy(1); 
   });
   it('does something else', () => {
-    expect(gcd(1709,1729)).toBe(1); 
+    expect(gcd(1709,1729)).toBeTruthy(1); 
   });
   it('does something else', () => {
-    expect(gcd(11,121)).toBe(11); 
+    expect(gcd(11,121)).toBeTruthy(11); 
   });
 })
