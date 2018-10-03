ffe96bee21cb7abb84ec6acedc3a39fc2720af91
Nicolas MOREAU <nicolas.moreau@efrei.net>
2018-10-03 11:09:53 +0200

    Implement isHarshad

--- src/isHarshad.js
+++ src/isHarshad.js
@@ -1,3 +1,10 @@
-export const isHarshad = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+import { sum } from "./sum";
+
+export const isHarshad = number => {
+  const digits = number
+    .toString()
+    .split("")
+    .map(Number);
+
+  return !(number % sum(digits));
 };
