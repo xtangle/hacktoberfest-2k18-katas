0b88fb59b10c8997e4adebd8cb5ecdee3b93a4fb
Lourdes Roashan <root.roashan@gmail.com>
2018-10-03 14:35:56 -0300

    Reverted back to original tests for PrimeFactors

--- src/primeFactors.test.js
+++ src/primeFactors.test.js
@@ -3,22 +3,22 @@ import { primeFactors } from './primeFactors';
 describe('primeFactors', () => {
 
     it('returns empty array for 1', () => {
-        expect(primeFactors(1)).toEqual([]);
+        expect(primeFactors(1)).toBeEqual([]);
     });
 
     it('returns only one primeFactor', () => {
-        expect(primeFactors(7)).toEqual([7]);
+        expect(primeFactors(7)).toBeEqual([7]);
     });
 
     it('return correct primeFactors', () => {
-        expect(primeFactors(4620)).toEqual([2, 2, 3, 5, 7, 11]);
+        expect(primeFactors(4620)).toBeEqual([2, 2, 3, 5, 7, 11]);
     });
 
     it('empty array for null', () => {
-        expect(primeFactors(null)).toEqual([]);
+        expect(primeFactors(null)).toBeEqual([]);
     });
 
     it('empty array for undefined', () => {
-        expect(primeFactors(undefined)).toEqual([]);
+        expect(primeFactors(undefined)).toBeEqual([]);
     });
 })
