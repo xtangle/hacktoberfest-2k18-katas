e6dadec0b56fc746c8a91adacf044a8ff482c236
Delkhaz <Delkhazibrahimi@gmail.com>
2018-10-02 22:40:00 -0500

    add impl for isHarshard.js

--- src/isHarshad.js
+++ src/isHarshad.js
@@ -1,3 +1,12 @@
-export const isHarshad = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const isHarshad = (num) => {
+	var sum = 0;
+	var str = new String(num);
+	for (var i=0; i<str.length; i++)
+		sum += parseInt(str.charAt(i));	
+
+	// sum of digits must be divisibly by the number
+	if(num % sum == 0)	
+		return true;
+	else
+		return false;
 };
