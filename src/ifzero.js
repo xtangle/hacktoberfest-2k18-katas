5332bbdaba7ccceb8fb8b9d6e3ca7d019d6fd69c
= <contact@m-mooga.com>
2018-10-03 10:36:15 +0200

    Revert "Revert "implement #77""
    This reverts commit b2a06591e0b2faaf0f33ec9a076f613afcf28c31.

--- src/ifzero.js
+++ src/ifzero.js
@@ -1,3 +1,21 @@
-export const ifzero = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const ifzero = (values) => {  
+  var sum = 0;
+  var isZero = false;
+  if(!Array.isArray(values) || !values.length){
+    return isZero;
+  }  
+
+  if(Array.isArray(values)){          
+    for (var j = 0; j < values.length; j++) {
+        if(typeof values[j] == 'undefined'){
+            break;
+        }            
+        sum += values[j];             
+    }        
+  }
+
+  if(sum == 0){
+    isZero = true;
+  }  
+return isZero;
 };
