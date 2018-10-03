0660e3582492e35f5c16fa3a1dc80549bf532953
honeyyenoh <aveselovalova@gmail.com>
2018-10-03 23:17:32 +0300

    #184: firstRecurringChar was implemented

--- src/firstRecurringChar.js
+++ src/firstRecurringChar.js
@@ -1,3 +1,11 @@
-export const firstRecurringChar = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const firstRecurringChar = (str) => {
+	if (typeof str != 'string') return null;
+	var char = "";
+	for (var s of str) {
+		if (char == s && char != "") {
+			return char; 
+		} else {
+			char = s;
+		}
+	}
 };
