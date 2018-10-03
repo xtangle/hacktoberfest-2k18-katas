4228d3a4e08fac87f3b11996766b16b94f7b3ead
kumaran-14 <kumarangowrisankar@yahoo.in>
2018-10-03 18:53:01 +0530

    implemented isPowerOfTwo, created primeFactors

---
+++ src/primeFactors.test.js
@@ -0,0 +1,24 @@
+import { primeFactors } from './primeFactors';
+
+describe('primeFactors', () => {
+  
+  it('returns empty array for 1', () => {
+    expect(primeFactors(1)).toBeEqual([]);
+  });
+
+  it('returns only one primeFactor', () => {
+    expect(primeFactors(7)).toBeEqual([7]);
+  });
+
+  it('return correct primeFactors', () => {
+    expect(primeFactors(4620)).toBeEqual([2, 2, 3, 5, 7, 11]);
+  });
+
+  it('empty array for null', () => {
+    expect(primeFactors(null)).toBeEqual([]);
+  });
+
+  it('empty array for undefined', () => {
+    expect(primeFactors(undefined)).toBeEqual([]);
+  });
+})
