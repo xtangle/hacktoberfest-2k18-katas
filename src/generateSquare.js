fe4d71783f9dc4aa7e99ef8d3c58844cad2cab01
Unknown <bunce.joe@gmail.com>
2018-10-03 21:21:13 +0100

    Complete the generateSquare kata and add a new one.
    Finished the generateSquare kata and added a new wordSpiral kata.

--- src/generateSquare.js
+++ src/generateSquare.js
@@ -1,3 +1,16 @@
-export const generateSquare = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const generateSquare = (sideLength) => {
+  switch (sideLength) {
+    case 0:
+      return [];
+    case 1:
+      return ['*'];
+    default:
+      var square = [];
+      square.push('*'.repeat(sideLength));
+      for (var i = 2; i < sideLength; i++) {
+        square.push('*' + ' '.repeat(sideLength - 2) + '*');
+      };
+      square.push('*'.repeat(sideLength));
+      return square;
+  }; 
 };
