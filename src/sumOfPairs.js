4126a686cc3b745660891022d84c0d566ad778ed
0c0de <gamesfreakin@gmail.com>
2018-10-03 19:13:26 +0200

    Added sumOfPairs kata

---
+++ src/sumOfPairs.js
@@ -0,0 +1,7 @@
+export const sumOfPairs = (arr) => {
+  /**
+   * Write a program that sum all the pairs from a starting number to an ending number 
+   * including starting and ending numbers.
+   * For example: from 0 to 6 ==> 0+2+4+6
+   */
+};
