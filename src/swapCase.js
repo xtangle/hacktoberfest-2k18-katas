738bd25c246d333d7cdda2cb814fd2b9109f5951
Andrey Semin <andreysemin@Andreys-MacBook-Pro.local>
2018-10-03 14:34:27 +0300

    add new kata

---
+++ src/swapCase.js
@@ -0,0 +1,3 @@
+export const swapCase = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
---
+++ src/swapCase.test.js
@@ -0,0 +1,15 @@
+import { swapCase } from './swapCase';
+
+describe('swapCase', () => {
+  test("swapCase of empty string to be empty string", () => {
+    expect(swapCase("")).toBe("");
+  });
+
+  test("swapCase of stR to be STr", () => {
+    expect(swapCase("stR")).toBe("STr");
+  });
+
+  test("swapCase of aBc#c1 to be AbC#C1", () => {
+    expect(swapCase("aBc#c1")).toBe("AbC#C1");
+  });
+})
