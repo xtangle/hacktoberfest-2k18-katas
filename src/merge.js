bb35a3610b19ef5b7062ff990b2d3ae2458a3958
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2018-10-03 15:12:27 +0200

    Implement merge

--- src/merge.js
+++ src/merge.js
@@ -1,3 +1,23 @@
-export const merge = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const merge = (...input) => {
+  if(input.length === 0){
+    return null;
+  }
+
+  if(input.length === 1){
+    return input[0];
+  }
+
+  if(input.length === 2){
+    let object1 = input[0];
+    let extended = input[1];
+    for(let prop in input[0]){
+        if(object1.hasOwnProperty(prop) && extended.hasOwnProperty(prop)){
+            extended[prop] = [object1[prop], extended[prop]];
+        }
+        else if(object1.hasOwnProperty(prop)){
+            extended[prop] = object1[prop];
+        }
+    }
+    return extended;
+  }
 };
---
+++ src/shortestWord.js
@@ -0,0 +1,3 @@
+export const shortestWord = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
