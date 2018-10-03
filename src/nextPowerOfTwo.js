e3d74bf3393f0916787a79cb45ce17ff20f695f8
lchaglla <lchaglla@maubic.es>
2018-10-03 21:31:12 +0200

    implemented nextPowerOfTwo

--- src/nextPowerOfTwo.js
+++ src/nextPowerOfTwo.js
@@ -1,3 +1,13 @@
-export const nextPowerOfTwo = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const nextPowerOfTwo = (a) => {
+  if (a <= 0) return 0;
+
+  let val = 0;
+  let i = 0;
+  
+	do {
+		val = Math.pow(2, i);
+		i++;
+  } while(val < a)
+  
+  return val;
 };
