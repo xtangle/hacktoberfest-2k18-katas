1e0b8d713ba43587163a1641a058191c410d449a
Renan GEHAN <rgehan94@gmail.com>
2018-10-03 19:41:40 +0200

    Touch unneeded file

--- src/sortNames.test.js
+++ src/sortNames.test.js
@@ -2,10 +2,18 @@ import { sortNames } from './sortNames';
 
 describe('sortNames', () => {
   it('the sorted names Nathan, Bob, Jessica should be Bob, Jessica, Nathan', () => {
-    expect(sortNames(["Nathan", "Bob", "Jessica"])).toEqual(["Bob", "Jessica", "Nathan"])
+    expect(sortNames(['Nathan', 'Bob', 'Jessica'])).toEqual([
+      'Bob',
+      'Jessica',
+      'Nathan',
+    ]);
   });
 
   it('the sorted names Jacob, Alice, Abbie should be Abbie, Alice, Jacob', () => {
-    expect(sortNames(["Jacob", "Alice", "Abbie"])).toEqual(["Abbie", "Alice", "Jacob"])
-  })
-})
+    expect(sortNames(['Jacob', 'Alice', 'Abbie'])).toEqual([
+      'Abbie',
+      'Alice',
+      'Jacob',
+    ]);
+  });
+});
