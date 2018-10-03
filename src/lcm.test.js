2e1136a0258c8e322d2cb01d0c78e9f3275faf91
mikek2 <mdk369@nyu.edu>
2018-10-03 19:57:56 -0400

    Fixed erroneous lcm test

--- src/lcm.test.js
+++ src/lcm.test.js
@@ -1,17 +1,17 @@
 import { lcm } from './lcm';
 
 describe('lcm', () => {
-  it('calculates least common multiple to be 5', () => {
-    expect(gcd(5,1000)).toEqual(5); 
+  it('calculates least common multiple to be 1000', () => {
+    expect(lcm(5,1000)).toEqual(1000); 
   });
 
-  it('calculates least common multiple to still be 5', () => {
-    expect(gcd(1000,5)).toEqual(1); 
+  it('calculates least common multiple to still be 1000', () => {
+    expect(lcm(1000,5)).toEqual(1000); 
   });
   it('calculates least common multiple to be null ', () => {
-    expect(gcd(0,1)).toBe(null); 
+    expect(lcm(0,1)).toBe(null); 
   });
-  it('calculates least common multiple to be 1', () => {
-    expect(gcd(9999,1)).toEqual(1); 
+  it('calculates least common multiple to be 9999', () => {
+    expect(lcm(9999,1)).toEqual(9999); 
   });
 })
