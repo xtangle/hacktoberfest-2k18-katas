2d01019e2ce9153051f8d46eb6f87c0156ca7100
Lourdes Roashan <root.roashan@gmail.com>
2018-10-03 10:16:52 -0300

    Implemented Product

--- src/product.js
+++ src/product.js
@@ -1,3 +1,11 @@
-export const product = () => {
+export const product = (arr) => {
   // TO IMPLEMENT IN ANOTHER PR
+  if (arr.length === 0) {
+    return 0;
+  }
+  else {
+    return arr.filter(prod => prod != null || prod != undefined).reduce((x, y) => x + y);
+  }
+
+
 };
