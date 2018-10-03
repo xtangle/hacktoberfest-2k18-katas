ea2059c1b6af24450e1d123fdb9b45bde972604f
kumaran-14 <kumarangowrisankar@yahoo.in>
2018-10-04 00:59:20 +0530

    Implement sieveOfEratosthenes, added firstRecurringChar

---
+++ src/firstRecurringChar.js
@@ -0,0 +1,3 @@
+export const firstRecurringChar = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
--- src/sieveOfEratosthenes.js
+++ src/sieveOfEratosthenes.js
@@ -1,3 +1,12 @@
-export const sieveOfEratosthenes = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const sieveOfEratosthenes = (arr) => {
+  if(Array.isArray(arr)) {
+    return arr.filter( el => ifPrime(el) )
+  }
+  return null
 };
+
+const ifPrime = num => {
+  for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+      if(num % i === 0) return false;
+  return num !== 1 && num !== 0;
+}
