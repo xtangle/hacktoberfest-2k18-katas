c765a90323e77da1e9e26adf9adfdc6f6d5d0293
Sergei Beregov <sergei.beregov@creditstar.com>
2018-10-04 21:05:07 +0300

    Update example

--- src/arrayLeftRotate.js
+++ src/arrayLeftRotate.js
@@ -1,7 +1,7 @@
 export const arrayLeftRotate = (arr, n) => {
   // move first element of array to the end of array n times
   // example: [1, 2, 3, 4], n = 2;
-  // steps: [1, 2, 3, 4] -> [2, 3, 4, 1] -> [3, 4, 1, 3]
-  // result: [1, 2, 3, 4]
+  // steps: [1, 2, 3, 4] -> [2, 3, 4, 1] -> [3, 4, 1, 2]
+  // result: [3, 4, 1, 2]
   // return null if first parameter is not array
 };
