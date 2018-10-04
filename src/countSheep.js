23a7fdfe64af9d21afe61ddacdfbf20834e3ec50
Sergei Beregov <sergei.beregov@creditstar.com>
2018-10-04 20:05:18 +0300

    Implement countSheep; add arrayLeftRotate

--- src/countSheep.js
+++ src/countSheep.js
@@ -2,4 +2,16 @@ export const countSheep = arr => {
   //Given an array as an argument
   //Should return the number of instances of the workd 'sheep'
   //Should return 0 if the sirst argument is not an array
+
+  if(!(arr instanceof Array)) {
+    return 0;
+  }
+
+  return arr.reduce(function(sheepCount, currentWord) {
+    if(currentWord === 'sheep') {
+      sheepCount += 1
+    }
+
+    return sheepCount;
+  }, 0)
 };
