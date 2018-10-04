9628d7edf91d45c25d0b8c649022e87bfe528b00
Delkhaz <Delkhazibrahimi@gmail.com>
2018-10-04 09:35:12 -0500

    removing isHarshad from commit hopefully

--- src/isHarshad.js
+++ src/isHarshad.js
@@ -1,10 +1,12 @@
-import { sum } from "./sum";
+export const isHarshad = (num) => {
+	var sum = 0;
+	var str = new String(num);
+	for (var i=0; i<str.length; i++)
+		sum += parseInt(str.charAt(i));	
 
-export const isHarshad = number => {
-  const digits = number
-    .toString()
-    .split("")
-    .map(Number);
-
-  return !(number % sum(digits));
+	// sum of digits must be divisibly by the number
+	if(num % sum == 0)	
+		return true;
+	else
+		return false;
 };
