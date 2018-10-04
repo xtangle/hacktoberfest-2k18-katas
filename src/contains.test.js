b4b470996bc94e74a0da664c07555108c1ea891f
devcer <viswanatham.santosh@gaiansolutions.com>
2018-10-04 17:36:21 +0530

    added test cases

--- src/contains.test.js
+++ src/contains.test.js
@@ -1,5 +1,11 @@
-import { contains } from './isObject';
+import { contains } from './contains';
 
 describe('contains', () => {
-
+  let arr = [1, 2, 3, 4, 5, 6];
+  it('should return true', () => {
+    expect(contains(arr, 3)).toEqual(true);
+  });
+  it('should return false', () => {
+    expect(contains(arr, 7)).toEqual(false);
+  })
 });
