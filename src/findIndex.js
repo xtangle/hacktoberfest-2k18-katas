ec641f3bb4147ae5dcb1dab1d6e819358d1d9bba
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2018-10-04 23:08:07 +0530

    completed findIndex and added kthSmallest problem

--- src/findIndex.js
+++ src/findIndex.js
@@ -1,6 +1,14 @@
-export const findIndex = () => {
-  // findIndex(array, predicate)
-  // returns the first index where the predicate truth test passes;
-  // otherwise returns -1.
-  // See the test to get some ideia of what the function has to do
+/**
+ * Finds the index of an item in an array if the item passes the predicate's condition
+ * @param {array} array Array to find index from
+ * @param {function} predicate predicate function to test each item of array
+ */
+export const findIndex = (array, predicate) => {
+  for (let i = 0, l = array.length; i < l; i++) {
+    const element = array[i];
+    if (predicate(element)) {
+      return i;
+    }
+  }
+  return -1;
 };
