b6173702e1df5698358a04a14b8b2e078aa0528f
Joao Julio <joao.tavares@novahaus.com.br>
2018-10-04 10:24:26 -0300

    Added new Kata findIndex

---
+++ src/findIndex.test.js
@@ -0,0 +1,19 @@
+import { findIndex } from './findIndex';
+
+const isPair = function(n) {
+  if (n % 2 === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+describe('findIndex', () => {
+  it('find first pair number', () => {
+    expect(findindex([1, 3, 4, 5], isPair)).toEqual(2); // TODO Your test instead
+  });
+
+  it('return an -1 when not found', () => {
+    expect(findindex([1, 3, 5, 7], isPair)).toEqual(-1); // TODO Your test instead
+  });
+})
