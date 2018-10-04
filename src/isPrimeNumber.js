44213f9dcb7a5278b00adda2143b0cf70619fbaa
lukebrobbs <lukemichaelvocalist@gmail.com>
2018-10-04 12:27:56 +0100

    Implement isPrimeNumber. Add reverseArray function with unit tests

--- src/isPrimeNumber.js
+++ src/isPrimeNumber.js
@@ -1,3 +1,10 @@
 export const isPrimeNumber = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+  if (n === 1) return false;
+  if (n === 2) return true;
+  for (let x = 2; x < n; x++) {
+    if (n % x === 0) {
+      return false;
+    }
+  }
+  return true;
 };
