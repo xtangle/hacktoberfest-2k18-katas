cd27d21e79d14ecd28b7176826a8aa658af0f5ee
Suzi Curran <suzicurran@gmail.com>
2018-10-04 12:54:51 -0400

    solve atbash

--- src/atbash.js
+++ src/atbash.js
@@ -1,3 +1,24 @@
-export const atbash = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const atbash = (string) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const alphabetCaps = alphabet.toUpperCase();
+  const alphabetCharacters = (alphabet + alphabetCaps).split('');
+  const reversedAlphabetCharacters = alphabet.split('').reverse().concat(
+    alphabetCaps.split('').reverse()
+  );
+
+  const characters = string.split('');
+
+  const encodedCharacters = characters.map(character => {
+    const isAlphabetic = alphabetCharacters.includes(character);
+
+    if (!isAlphabetic) {
+      return character;
+    }
+
+    const index = alphabetCharacters.findIndex(element => element === character);
+    return reversedAlphabetCharacters[index];
+
+  });
+
+  return encodedCharacters.join('');
 };
