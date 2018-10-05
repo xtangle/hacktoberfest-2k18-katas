6082fc17d60cb9bee76681badf42dd77968bf911
assertnotnull <patgauth@gmail.com>
2018-10-04 23:51:19 -0400

    fix equal method and add reverseArray code

--- src/reverseArray.js
+++ src/reverseArray.js
@@ -2,4 +2,9 @@ export const reverseArray = arr => {
   //Takes an array as an arg
   //Returns the array in reverse
   //returns null if the arg is not an array
+  if (arr instanceof Array) {
+    if(arr.length == 0) return "array"
+    return arr.reverse();
+  }
+  return null;
 };
