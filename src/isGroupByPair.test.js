a308c99bcaef943b0e9bce696c3eb8688fc3972a
Banb4n <alban.such@gmail.com>
2018-10-05 23:53:06 +0200

    Fix tests

--- src/isGroupByPair.test.js
+++ src/isGroupByPair.test.js
@@ -2,22 +2,22 @@ import { isGroupByPair } from './isGroupByPair';
 
 describe('isGroupByPair', () => {
   it('Should return true', () => {
-    expect('[]').toBeTruthy(); // TODO Your test instead
+    expect(isGroupByPair('[]')).toBeTruthy(); // TODO Your test instead
   });
 
   it('Should return true', () => {
-    expect('{}').toBeTruthy(); // TODO Your test instead
+    expect(isGroupByPair('{}')).toBeTruthy(); // TODO Your test instead
   });
 
   it('Should return true', () => {
-    expect('()').toBeTruthy(); // TODO Your test instead
+    expect(isGroupByPair('()')).toBeTruthy(); // TODO Your test instead
   });
 
   it('Should return false', () => {
-    expect('{}[])').toBe(false); // TODO Your test instead
+    expect(isGroupByPair('{}[])')).toBe(false); // TODO Your test instead
   });
 
   it('Should return false', () => {
-    expect('{[)]}').toBe(false); // TODO Your test instead
+    expect(isGroupByPair('{(}[])')).toBe(false); // TODO Your test instead
   });
 })
