eb5eb68f6719fad05ec3379a2a05bf6c7dc77d85
mannutech <hiteshgoel.afs@gmail.com>
2018-10-05 15:57:51 +0530

    implemented romanToInteger

---
+++ src/arrayElementsSubtraction.test.js
@@ -0,0 +1,16 @@
+import { arrayElementsSubtraction } from './arrayElementsSubtraction';
+
+describe('arrayElementsSubtraction', () => {
+  it('should return difference between 2 arrays elements as a separate array', () => {
+    expect(arrayElementsSubtraction([1, 2, 3, 4], [1, 2, 3, 4])).toEqual([0, 0, 0, 0]);
+  });
+  it('should return difference between 2 arrays elements as a separate array', () => {
+    expect(arrayElementsSubtraction([0, 0, 0, 0], [1, 2, 3, 4])).toEqual([-1, -2, -3, -4]);
+  });
+  it('should return difference between 2 arrays elements as a separate array', () => {
+    expect(arrayElementsSubtraction([-1, -2, -3, -4], [1, 2, 3, 4])).toEqual([-2, -4, -6, -8]);
+  });
+  it('should throw error on unequal arrays or non numeric array', () => {
+    expect(arrayElementsSubtraction(['1', '2', '3', '4'], ['a', 'b', 'c'])).toThrow();;
+  });
+})
