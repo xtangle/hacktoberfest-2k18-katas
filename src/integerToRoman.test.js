9c198665734778550eba6a1426caddefa502f4b8
mannutech <hiteshgoel.afs@gmail.com>
2018-10-05 15:23:28 +0530

    added test cases integerToRoman

--- src/integerToRoman.test.js
+++ src/integerToRoman.test.js
@@ -1,11 +1,19 @@
 import { integerToRoman } from './integerToRoman';
 
 describe('integerToRoman', () => {
-  it('does something', () => {
-    expect(true).toBeTruthy(); // TODO Your test instead
+  it('should convert 1 to I', () => {
+    expect(integerToRoman(1)).toBe("I");
   });
 
-  it('does something else', () => {
-    expect(true).toBeTruthy(); // TODO Your test instead
+  it('should convert 4 to IV', () => {
+    expect(integerToRoman(4)).toBe("IV");
+  });
+
+  it('should convert 11 to XI', () => {
+    expect(integerToRoman(11)).toBe("XI");
+  });
+
+  it('should convert 3999 to MMMCMXCIX', () => {
+    expect(integerToRoman(3999)).toBe("MMMCMXCIX");
   });
 })
