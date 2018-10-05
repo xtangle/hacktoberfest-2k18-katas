b8e0b23ce6d35073e6c1008a64644aae53661e6a
Renan GEHAN <rgehan94@gmail.com>
2018-10-05 11:35:39 +0200

    Update decimalToOctal.test.js

--- src/decimalToOctal.test.js
+++ src/decimalToOctal.test.js
@@ -2,17 +2,17 @@ import { decimalToOctal } from './decimalToOctal';
 
 describe('decimalToOctal', () => {
   it('return correct value of number provided', () => {
-    expect(decimalToOctal(100)).toEqual('144'); // TODO Your test instead
+    expect(decimalToOctal(100)).toEqual('144');
   });
 
   it('throws error when passed non decimal value', () => {
-    expect(decimalToOctal(null)).toThrow(); // TODO Your test instead
+    expect(decimalToOctal(null)).toThrow();
   });
   it('throws error when passed non decimal value', () => {
-    expect(decimalToOctal('1223RandomString')).toThrow(); // TODO Your test instead
+    expect(decimalToOctal('1223RandomString')).toThrow();
   });
 
   it('return octal value of a number passed with a precision', () => {
-    expect(decimalToOctal(100.43453)).toEqual('144.33636533526206'); // TODO Your test instead
+    expect(decimalToOctal(100.43453)).toEqual('144.33636533526206');
   });
 })
