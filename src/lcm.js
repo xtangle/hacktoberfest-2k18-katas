87a6ba232a18ed4832c4548a4b04e504ff4de9ef
Delkhaz <Delkhazibrahimi@gmail.com>
2018-10-05 11:24:29 -0500

    lcm.js implemented

--- src/lcm.js
+++ src/lcm.js
@@ -1,3 +1,11 @@
-export const lcm = () => {
-  // TO IMPLEMENT IN ANOTHER PR
-};
+export const lcm = (x,y) => {
+	//lcm (x,y) = (x*y)/gcd(x,y)
+	function gcd (x, y){
+		if (y == 0) 
+			return x;
+		return gcd(y, x%y);
+	}
+	if (x == 0 || y == 0)
+		return null;
+	return (x*y)/gcd(x,y);
+};	
