7630047ab727b87ab47f538be6dd5a6dfebe8145
Jan Czizikow <jan.czizikow@gmail.com>
2018-10-05 15:09:42 +0200

    Add a solution for arrayElementsSubstraction

--- src/arrayElementsSubtraction.js
+++ src/arrayElementsSubtraction.js
@@ -1,3 +1,10 @@
-export const arrayElementsSubtraction = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const arrayElementsSubtraction = (a, b) => {
+  if (a.length !== b.length) throw new Error("Array's lengths are unequal");
+  return a.map((el, i) => {
+    if (typeof el === 'number' && typeof b[i] === 'number') {
+      return el - b[i];
+    } else {
+      throw new Error('Array values should be a number');
+    }
+  });
 };
