7852415473979ede2b15b69706d8b360df207910
mannutech <hiteshgoel.afs@gmail.com>
2018-10-05 15:19:17 +0530

    implemented romanToInteger,

--- src/integerToRoman.js
+++ src/integerToRoman.js
@@ -1,18 +1,3 @@
-export const integerToRoman = (num) => {
-	var answer = '';
-  //array for both roman and decimal value
-  //4,9,40 special cases 
-  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
-  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  var decimal_length = decimal.length;
-
-  for (var i = 0; i <= decimal_length; i++) {
-  // when we find a num less than our num, subtract it from our num and repeat process
-    while (num % decimal[i] < num) {   
-      answer += roman[i];
-      //if we found a matching, a roman numerial will be added to our answer
-      num -= decimal[i];
-    }
-  }
-  return answer;
+export const integerToRoman = () => {
+  // TO IMPLEMENT IN ANOTHER PR
 };
--- src/integerToRoman.test.js
+++ src/integerToRoman.test.js
@@ -1,19 +1,11 @@
 import { integerToRoman } from './integerToRoman';
 
 describe('integerToRoman', () => {
-  it('should convert 1 to I', () => {
-    expect(integerToRoman(1)).toBe("I");
+  it('does something', () => {
+    expect(true).toBeTruthy(); // TODO Your test instead
   });
 
-  it('should convert 4 to IV', () => {
-    expect(integerToRoman(4)).toBe("IV");
-  });
-
-  it('should convert 11 to XI', () => {
-    expect(integerToRoman(11)).toBe("XI");
-  });
-
-  it('should convert 3999 to MMMCMXCIX', () => {
-    expect(integerToRoman(3999)).toBe("MMMCMXCIX");
+  it('does something else', () => {
+    expect(true).toBeTruthy(); // TODO Your test instead
   });
 })
