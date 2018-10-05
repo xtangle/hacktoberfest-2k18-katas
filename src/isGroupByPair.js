8aa74032f345ac514f8d923401f5220ec240ac78
Banb4n <alban.such@gmail.com>
2018-10-05 23:30:49 +0200

    Add new kata isGroupByPair

---
+++ src/isGroupByPair.js
@@ -0,0 +1,3 @@
+export const isGroupByPair = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
---
+++ src/isGroupByPair.test.js
@@ -0,0 +1,23 @@
+import { isGroupByPair } from './isGroupByPair';
+
+describe('isGroupByPair', () => {
+  it('Should return true', () => {
+    expect('[]').toBeTruthy(); // TODO Your test instead
+  });
+
+  it('Should return true', () => {
+    expect('{}').toBeTruthy(); // TODO Your test instead
+  });
+
+  it('Should return true', () => {
+    expect('()').toBeTruthy(); // TODO Your test instead
+  });
+
+  it('Should return false', () => {
+    expect('{[()]}').toBe(false); // TODO Your test instead
+  });
+
+  it('Should return false', () => {
+    expect('{[)]}').toBe(false); // TODO Your test instead
+  });
+})
