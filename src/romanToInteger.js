eed38ca9fd3d9b57084249b27e29639c520164dc
mannutech <hiteshgoel.afs@gmail.com>
2018-10-05 15:46:07 +0530

    Revert "implemented romanToInteger,"
    This reverts commit 7852415473979ede2b15b69706d8b360df207910.

--- src/romanToInteger.js
+++ src/romanToInteger.js
@@ -1,30 +1,3 @@
-export const romanToInteger = (romanString) => {
-  if (romanString == null) return -1;
-  let num = char_to_int(romanString.charAt(0));
-  let previousChar, currentChar;
-
-  for (let i = 1; i < romanString.length; i++) {
-    currentChar = char_to_int(romanString.charAt(i));
-    previousChar = char_to_int(romanString.charAt(i - 1));
-    if (currentChar <= previousChar) {
-      num += currentChar;
-    } else {
-      num = num - previousChar * 2 + currentChar;
-    }
-  }
-
-  return num;
-
-  function char_to_int(c) {
-    switch (c) {
-      case 'I': return 1;
-      case 'V': return 5;
-      case 'X': return 10;
-      case 'L': return 50;
-      case 'C': return 100;
-      case 'D': return 500;
-      case 'M': return 1000;
-      default: return -1;
-    }
-  }
+export const romanToInteger = () => {
+  // TO IMPLEMENT IN ANOTHER PR
 };
