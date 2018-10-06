179ecab6a73e0d0de58c7abb39b14c10e55eb700
Pynnl <ryanvu.programmer@outlook.com>
2018-10-06 02:05:19 -0400

    implementation + kata

--- src/evalOrQuery.js
+++ src/evalOrQuery.js
@@ -1,3 +1,21 @@
+const Q = {
+  '<': -1,
+  '=': 0,
+  '>': 1
+}
+
 export const evalOrQuery = (arr, query, comparator) => {
-  // TO IMPLEMENT IN ANOTHER PR
+  if (!query || !Object.keys(query).length) return arr
+
+  let rt = []
+  for (let e of arr) {
+    for (let q in query) {
+      if (Q[q] == comparator(e, query[q])) {
+        rt.push(e)
+        break
+      }
+    }
+  }
+
+  return rt
 };
