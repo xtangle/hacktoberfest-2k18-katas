0ebc6a19d1c9f59045995ac18fbb94b171fe449b
Adarsh <adarshan_naiynar@icloud.com>
2018-10-06 23:29:00 +0530

    Implemented setObjectValueAtPath and added deleteObjectKeyAtPath

--- src/setObjectValueAtPath.js
+++ src/setObjectValueAtPath.js
@@ -1,3 +1,15 @@
-export const setObjectValueAtPath = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const setObjectValueAtPath = (obj, path, value) => {
+  if (!obj || !path) {
+    throw new Error();
+  }
+  const splitPath = path.split('.');
+  const key = splitPath.shift();
+  if (!obj.hasOwnProperty(key)) {
+    obj[key] = {};
+  }
+  if (splitPath.length === 0) {
+    obj[key] = value;
+    return obj;
+  }
+  return setObjectValueAtPath(obj[key], splitPath.join('.'), value);
 };
