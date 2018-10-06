034c4f67608a4c0f7e82540e2ab153468c127db4
Pynnl <ryanvu.programmer@outlook.com>
2018-10-06 16:00:19 -0400

    implementation + kata

---
+++ src/centerText.js
@@ -0,0 +1,7 @@
+// Given a string 's' and a length 'n', split 's' into
+// strings which have exactly 'n' characters. Center the strings
+// and fill with spaces. If the string cannot be completely centered,
+// the left side is prefered. Return array of proceeded strings.
+export const centerText = (s, n) => {
+  // TO IMPLEMENT IN ANOTHER PR
+}
--- src/longestUniqueSubstring.js
+++ src/longestUniqueSubstring.js
@@ -1,2 +1,15 @@
-export const longestUniqueSubstring = (string) => {
+export const longestUniqueSubstring = s => {
+  if (typeof s != 'string') throw 'Input should be a string'
+
+  let lus = '' //longest unique substring
+
+  for (let i = 1, start = 0; i <= s.length; i++) {
+    if (s.charCodeAt(i) - s.charCodeAt(i - 1) != 1) {
+      let us = s.substring(start, i)
+      if (us.length > lus.length) lus = us
+      start = i
+    }
+  }
+
+  return lus
 };
