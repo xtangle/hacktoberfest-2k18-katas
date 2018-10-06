1970ec4eccfe4caa0a156abdc282bb559c709001
Michal Wagner <mikebudimon@gmail.com>
2018-10-07 01:07:58 +0200

    Implement arrayLeftRotate, add arrayRightRotate
    - Impement arrayLeftRotate, fix arrayLeftRotate.test
    - Add arrayRightRotate and arrayRightRotate.test

--- src/arrayLeftRotate.js
+++ src/arrayLeftRotate.js
@@ -1,7 +1,10 @@
 export const arrayLeftRotate = (arr, n) => {
-  // move first element of array to the end of array n times
-  // example: [1, 2, 3, 4], n = 2;
-  // steps: [1, 2, 3, 4] -> [2, 3, 4, 1] -> [3, 4, 1, 2]
-  // result: [3, 4, 1, 2]
-  // return null if first parameter is not array
+  if (Array.isArray(arr)) {
+    for (let index = 0; index < n; index++) {
+      arr.push(arr.shift());
+    }
+    return arr;
+  } else {
+    return null;
+  }
 };
--- src/arrayLeftRotate.test.js
+++ src/arrayLeftRotate.test.js
@@ -6,10 +6,10 @@ describe('arrayLeftRotate', () => {
   });
 
   it('left rotate [5, 6, 3, 4] two times should return [3, 4, 5, 6]', () => {
-    expect(arrayLeftRotate([5, 6, 3, 4], 2), 2).toEqual([3, 4, 5, 6]);
+    expect(arrayLeftRotate([5, 6, 3, 4], 2)).toEqual([3, 4, 5, 6]);
   });
 
-  it("return null if first parameter is not array", () => {
-    expect(arrayLeftRotate("hello", 3)).toEqual(0);
+  it('return null if first parameter is not array', () => {
+    expect(arrayLeftRotate('hello', 3)).toEqual(null);
   });
-})
+});
