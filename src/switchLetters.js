d2538c5e2a3fa6f5bb1b53b1cb405c72ca2a7b14
Christian Dohnert <donut87@gmail.com>
2018-10-07 12:59:41 +0200

    implement switchLetters

--- src/switchLetters.js
+++ src/switchLetters.js
@@ -1,3 +1,16 @@
-export const switchLetters = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const switchLetters = (inputString) => {
+  const wordList = inputString.split(/(?=[A-Z0-9])/);
+  const switchedWordList = wordList.map(word => switchBeginningAndEnd(word)).map(word => word.toLowerCase())
+
+  return switchedWordList.join('')
 };
+
+const switchBeginningAndEnd = (word) => {
+  if(word.length === 1) {
+    return word
+  }
+  const firstLetter = word.slice(0,1)
+  const lastLetter = word.slice(-1)
+  const wordCenter = word.slice(1, -1)
+  return lastLetter + wordCenter + firstLetter
+}
