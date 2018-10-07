b71f2116fd17d625e67353821c31e7ddc35d579a
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2018-10-07 02:48:26 +0200

    Add abbreviateString.js and octalToDecimal.test.js

--- src/abbreviateString.js
+++ src/abbreviateString.js
@@ -1,3 +1,12 @@
-export const abbreviateString = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const abbreviateString = (str) => {
+	if (typeof str !== 'string')
+		throw 'type not permitted'
+	
+	let strArray = str.split(' ')
+	let lenArray = strArray.length
+	
+	if (lenArray >= 2)
+		return strArray[0] + ' ' + strArray[lenArray-1][0].toUpperCase() + '.'
+	else
+		return strArray[0]
 };
