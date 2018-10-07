643034c32f9af20f32124c5f4c55eb7861ee7012
jrga03 <jasonacido@gmail.com>
2018-10-07 23:47:20 +0800

    finish stringToBinary and add new kata

---
+++ src/removeItems.test.js
@@ -0,0 +1,26 @@
+import { removeItems } from './removeItems';
+
+describe('removeItems', () => {
+  it('returns undefined with invalid arguments', () => {
+    expect(removeItems()).toBeUndefined();
+    expect(removeItems('foo')).toBeUndefined();
+    expect(removeItems(100)).toBeUndefined();
+    expect(removeItems(null)).toBeUndefined();
+    expect(removeItems({ bar: 'baz' })).toBeUndefined();
+    expect(removeItems(true)).toBeUndefined();
+    expect(removeItems(undefined)).toBeUndefined();
+  });
+
+  it('removeItems to remove (1, 2, 3, 4, 5) from [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
+    expect(removeItems([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1, 2, 3, 4, 5)).toEqual([6, 7, 8, 9, 10]);
+    expect(removeItems(['Pete', 'Andrew', 'Michelle', 'Alex'], 'Andrew', 'Michelle')).toEqual(['Pete', 'Alex']);
+  });
+
+  it('removeItems to remove nothing if only one argument is given', () => {
+    expect(removeItems(['foo', 'bar'])).toEqual(['foo', 'bar']);
+  });
+
+  it('removeItems to remove nothing if no items match with the array', () => {
+    expect(removeItems(['burger', 'fries', 'soda'], 'apple', 'banana')).toEqual(['burger', 'fries', 'soda']);
+  });
+});
--- src/stringToBinary.js
+++ src/stringToBinary.js
@@ -1,3 +1,10 @@
-export const stringToBinary = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const stringToBinary = ( string ) => {
+  if ( !string || string === ' ' ) {
+    return null;
+  }
+  let result = [];
+  for (let i = 0; i < string.length; i++) {
+    result.push( string.charCodeAt( i ).toString( 2 ).padStart( 8, '0' ));
+  }
+  return result.join( ' ' );
 };
