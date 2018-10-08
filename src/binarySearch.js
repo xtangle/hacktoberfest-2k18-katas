40873e916a4d2c431a4a805cd2b4ba791df188b0
Deshpande, Mandar <Mandar_Deshpande@intuit.com>
2018-10-08 13:53:18 -0700

    Adding implementation for binarySearch and added rotateString

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -1,3 +1,22 @@
-export const binarySearch = (arr) => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const binarySearch = (arr, target) => {
+
+  let startIndex = 0;
+  let endIndex = arr.length - 1;
+
+  while( startIndex <= endIndex ){
+
+    let middle = Math.floor((startIndex + endIndex )/2);
+
+    if( arr[middle] < target){
+      startIndex = middle -1;
+    } else if ( arr[middle] > target){
+      endIndex = middle + 1;
+    } else {
+      return middle
+    }
+
+  }
+
+  return -1;
+
 };
---
+++ src/rotateString.test.js
@@ -0,0 +1,34 @@
+import { rotateString } from './rotateString';
+
+/*
+ * Example of the assertions you can use for your tests:
+ *
+ * - Testing equality between the function's output and a constant:
+ * expect(rotateString()).toEqual(123);
+ *
+ * - Testing that the function returns null:
+ * expect(rotateString()).toBeNull();
+ *
+ * - Testing that the function returns a falsy value (eg. false, 0, "")
+ * expect(rotateString()).toBeFalsy();
+ *
+ * - Testing that the function returns a truthy value (eg. true, 1, "abc")
+ * expect(rotateString()).toBeTruthy();
+ *
+ * - Testing that the function throws
+ * expect(() => { rotateString(); }).toThrow();
+ */
+
+describe('rotateString', () => {
+  it('rotateString does this thing...', () => {
+    // TODO Your own assertion here
+  });
+
+  it('rotateString does that other thing...', () => {
+    // TODO Your own assertion here
+  });
+
+  it('rotateString does a very cool thing...', () => {
+    // TODO Your own assertion here
+  });
+});
