fe2f9428025900be95d39199adb7881cac9e33aa
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2018-10-08 23:38:26 +0530

    minor formatting corrections. added another kata: doYouKnowDaWae

--- src/combinationModPrime.js
+++ src/combinationModPrime.js
@@ -1,16 +1,16 @@
 export const combinationModPrime = (n, k) => {
-  // TO IMPLEMENT IN ANOTHER PR
-  const MOD = 1000000007
+	// TO IMPLEMENT IN ANOTHER PR
+	const MOD = 1000000007
 
-  let orow = [1, 1]
-  let ans = 0
-  
-  for(let i = 2; i <= n; ++i) {
-  	let nrow = [...Array(i + 1)].map(_ => 0)
-  	for(let j  = 0; j <= i; ++j) {
-  		nrow[j] = (j === 0 || j === i ? 1 : orow[j] + orow[j - 1])%MOD
-  	}
-  	orow = nrow
-  }
-  return orow[k]
+	let orow = [1, 1]
+	let ans = 0
+
+	for (let i = 2; i <= n; ++i) {
+		let nrow = [...Array(i + 1)].map(_ => 0)
+		for (let j = 0; j <= i; ++j) {
+			nrow[j] = (j === 0 || j === i ? 1 : orow[j] + orow[j - 1]) % MOD
+		}
+		orow = nrow
+	}
+	return orow[k]
 };
