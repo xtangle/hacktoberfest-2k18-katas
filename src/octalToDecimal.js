dc2bf05339e08954802a7514d804a7bb5d61f5e4
Tenk Milan <tenkmilan@gmail.com>
2018-10-08 20:55:09 +0200

    OctalToDecimal solution added, kelvinToCelsius test added

--- src/octalToDecimal.js
+++ src/octalToDecimal.js
@@ -1,3 +1,20 @@
-export const octalToDecimal = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const octalToDecimal = (octalNum) => {
+  if(!isNaN(octalNum) && typeof octalNum === 'string') {
+    let splitted = octalNum.split('.');
+    let integer = parseInt(splitted[0], 8);
+    if(splitted.length == 2) {
+      let fractionalElements = splitted[1].split('');
+      let powVal = -1;
+      let fractSum = 0;
+      fractionalElements.forEach(element => {
+        fractSum += parseInt(element)*Math.pow(8, powVal);
+        powVal--;
+      });
+      return integer + fractSum;
+    } else {
+      return integer;
+    }
+  } else {
+    throw "Input is invalid";
+  }
 };
