a312d8a61bca0f45ed3304241785b037f1326ad4
Deshpande, Mandar <Mandar_Deshpande@intuit.com>
2018-10-08 14:21:44 -0700

    Fixing a small bug

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -2,15 +2,16 @@ export const binarySearch = (arr, target) => {
 
   let startIndex = 0;
   let endIndex = arr.length - 1;
+  let middle = 0;
 
   while( startIndex <= endIndex ){
 
-    let middle = Math.floor((startIndex + endIndex )/2);
+    middle = Math.floor((startIndex + endIndex )/2);
 
     if( arr[middle] < target){
-      startIndex = middle -1;
+      startIndex = middle + 1;
     } else if ( arr[middle] > target){
-      endIndex = middle + 1;
+      endIndex = middle - 1;
     } else {
       return middle
     }
