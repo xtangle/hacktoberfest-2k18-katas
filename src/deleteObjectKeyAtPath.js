6b474bd5a8e28cfc7cf8d23cdfbff699a86a2a15
Izra Faturrahman <Frizz925@hotmail.com>
2018-10-08 11:45:49 +0700

    Add deleteObjectKeyAtPath function

--- src/deleteObjectKeyAtPath.js
+++ src/deleteObjectKeyAtPath.js
@@ -1,3 +1,15 @@
-export const deleteObjectKeyAtPath = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const deleteObjectKeyAtPath = (obj, path) => {
+  const keys = path.split('.');
+  const lastKey = keys.pop();
+  let target = obj;
+  while (target && keys.length > 0) {
+    const key = keys.shift();
+    target = target[key];
+  }
+  if (target && typeof target === 'object' && target[lastKey]) {
+    delete target[lastKey];
+  } else {
+    throw new Error('Path not found!');
+  }
+  return obj;
 };
