00cd2569f7312583aee4dce479c2f2c246e31244
Deshpande, Mandar <Mandar_Deshpande@intuit.com>
2018-10-08 14:09:21 -0700

    Adding test cases for rotate string

--- src/rotateString.test.js
+++ src/rotateString.test.js
@@ -1,34 +1,12 @@
 import { rotateString } from './rotateString';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(rotateString()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(rotateString()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(rotateString()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(rotateString()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { rotateString(); }).toThrow();
- */
 
 describe('rotateString', () => {
-  it('rotateString does this thing...', () => {
-    // TODO Your own assertion here
+  it('rotateString returns true for the given strings', () => {
+    expect(rotateString('abcde','cdeab')).toBeTruthy();
   });
 
-  it('rotateString does that other thing...', () => {
-    // TODO Your own assertion here
-  });
-
-  it('rotateString does a very cool thing...', () => {
-    // TODO Your own assertion here
+  it('rotateString returns false for the given strings', () => {
+    expect(rotateString('abcde','abcde')).toBeFalsy();
   });
 });
