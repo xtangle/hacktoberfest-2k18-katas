cdabe6bb44320c992e13b414af50eeeb82e2e31c
Bharath Kumar <kumarbharath718@gmail.com>
2018-10-09 16:11:05 +0530

    Implement arrayAverage

--- src/arrayAverage.js
+++ src/arrayAverage.js
@@ -1,3 +1,8 @@
-export const arrayAverage = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const arrayAverage = (arr) => {
+  let sum = 0, avg = 0;
+  if(arr.length) {
+    sum = arr.reduce((a, b) => { return a+b });
+    avg = sum / arr.length;
+  }
+  return avg;
 }
