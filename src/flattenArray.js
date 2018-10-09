61eec90f8cebe6a3c52962d566d683b571cbf88a
Cesar Boaventura <cesar.boaventura61@gmail.com>
2018-10-09 18:27:14 -0300

    implemented flattenArray function

--- src/flattenArray.js
+++ src/flattenArray.js
@@ -1,3 +1,7 @@
-export const flattenArray = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const flattenArray = arr => {
+  const parsedArray = JSON.parse(arr);
+
+  return JSON.stringify(
+    parsedArray.reduce((prev, curr) => prev.concat(curr), []),
+  );
 };
