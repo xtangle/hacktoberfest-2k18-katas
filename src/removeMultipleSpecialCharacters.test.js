561eee24bc89fcccaaf6d9a296e5636861541531
Shawn <sppang1@yahoo.com>
2018-10-08 20:27:15 -0400

    fixed removeMultipleSpecialCharacters test

--- src/removeMultipleSpecialCharacters.test.js
+++ src/removeMultipleSpecialCharacters.test.js
@@ -2,8 +2,14 @@ import { removeMultipleSpecialCharacters } from './removeMultipleSpecialCharacte
 
 describe('removeMultipleSpecialCharacters', () => {
   it('should return the number of words in the sentence', () => {
-    expect(getWordCount('One, Two, Three, Four')).toEqual(4);
-    expect(getWordCount('You! Got! It! Right!')).toEqual(4);
-    expect(getWordCount('Can!@# you$%^ read&*() this?')).toEqual(4);
+    expect(removeMultipleSpecialCharacters('One, Two, Three, Four')).toEqual(4);
+  });
+
+  it('should return the number of words in the sentence', () => {
+    expect(removeMultipleSpecialCharacters('You! Got! It! Right!')).toEqual(4);
+  });
+
+  it('should return the number of words in the sentence', () => {
+    expect(removeMultipleSpecialCharacters('Can!@# you$%^ read&*() this?')).toEqual(4);
   });
 });
