89ee22826acfb0a82fd80eabbe51a3cf3ba53af8
Zeroji <zzeroji@gmail.com>
2018-10-09 19:08:24 +0200

    Implement trailingZeroes #518

--- src/trailingZeroes.js
+++ src/trailingZeroes.js
@@ -1,3 +1,10 @@
-export const trailingZeroes = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const trailingZeroes = (number) => {
+  if(!Number.isInteger(number) || number < 0) throw(TypeError);
+  var zeroes = 0;
+  var base = 5;
+  while(base < number) {
+    zeroes += Math.floor(number / base);
+    base *= 5;
+  }
+  return zeroes;
 };
