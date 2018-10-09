5ead0f5604638b2ed9ee79351ead10cca545195a
Jason Ray Acido <jasonacido@gmail.com>
2018-10-09 22:05:06 +0800

    implement collatzFunction and add isDollar kata

--- src/collatzFunction.js
+++ src/collatzFunction.js
@@ -1,3 +1,23 @@
-export const collatzFunction = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const collatzFunction = ( number ) => {
+  if ( !Number.isInteger( number ) || number <= 0 ) {
+    return -1;
+  }
+
+  let count = 0;
+
+  (function getCollatz( num ) {
+    if ( num === 1 ) {
+      return;
+    } else if ( num % 2 > 0 ) {
+      // Odd
+      count++;
+      getCollatz(( num * 3 ) + 1 );
+    } else {
+      // Even
+      count++;
+      getCollatz( num / 2 );
+    }
+  })( number )
+
+  return count;
 };
---
+++ src/isDollar.js
@@ -0,0 +1,3 @@
+export const isDollar = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
