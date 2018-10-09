21512ef117abf733841bfb1af7e494c3eff372cf
Cesar Boaventura <cesar.boaventura61@gmail.com>
2018-10-09 19:01:18 -0300

    added more tests case

--- src/duplicatedArgs.test.js
+++ src/duplicatedArgs.test.js
@@ -3,9 +3,25 @@ import { duplicatedArgs } from './duplicatedArgs';
 describe('duplicatedArgs', () => {
   test('should return true when duplicated', () => {
     expect(duplicatedArgs('first', 'first')).toEqual(true);
+    expect(
+      duplicatedArgs(
+        'abc',
+        'abc',
+        'abc',
+        '123',
+        'abc',
+        '123',
+        'abc',
+        'abc',
+        '123',
+      ),
+    ).toEqual(true);
   });
 
   test('should return false when not duplicated', () => {
     expect(duplicatedArgs('first', 'second')).toEqual(false);
+    expect(
+      duplicatedArgs('first', 'second', 'third', 'fourth', 'fifth', 'sixth'),
+    ).toEqual(false);
   });
 });
