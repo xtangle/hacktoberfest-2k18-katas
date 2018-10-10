44827a880427d0063ccf49269458ae7d1af0daa9
Jo Haugum <johaugum@Jos-MacBook-Pro-2.local>
2018-10-10 00:40:23 -0400

    implement numWaysClimbStaircases

--- src/numWaysClimbStaircase.js
+++ src/numWaysClimbStaircase.js
@@ -1,3 +1,20 @@
+function countWays (ways, stepSizes) {
+    if (ways <= 1) { 
+        return ways; 
+    }
+  
+    var res = 0;
+    var i = 1;
+
+    while (i <= ways && i <= stepSizes) {
+        res += countWays(ways-i, stepSizes);
+        i++;
+    }
+
+    return res;
+}
+
 export const numWaysClimbStaircase = (ways, stepSizes = [1, 2]) => {
-  // TO IMPLEMENT IN ANOTHER PR
+    var max = stepSizes[stepSizes.length-1];
+    return countWays(ways+1, max);
 }
