4edc82fb4031cf608906a72f894628899d4e200c
Anderson Souza <andersom.eugenio@gmail.com>
2018-10-10 10:34:31 -0300

    add implementation

--- src/numberKeysInJSON.js
+++ src/numberKeysInJSON.js
@@ -1,3 +1,21 @@
-export const numberKeysInJSON = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+function isObject(obj) {
+  return typeof obj === "object" && Object.keys(obj).length;
+}
+
+function countKeys(obj) {
+  if (isObject(obj)) {
+    return (
+      Object.keys(obj).reduce((acum, key) => acum + countKeys(obj[key]), 0) + 1
+    );
+  } else {
+    return 1;
+  }
+}
+
+export const numberKeysInJSON = obj => {
+  if (isObject(obj)) {
+    return countKeys(obj) - 1;
+  } else {
+    throw new Exception();
+  }
 };
