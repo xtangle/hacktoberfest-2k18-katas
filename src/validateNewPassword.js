228e6095a0fed26a505b9f8576bc70039a29d4b8
Boris Kubiak <kubiakboris@gmail.com>
2018-10-10 12:09:51 +0200

    Implement validateNewPassword

--- src/validateNewPassword.js
+++ src/validateNewPassword.js
@@ -4,6 +4,27 @@
 //   - 1 lowercase
 //   - 1 number
 //   - 1 special character (not 0-9, a-z, A-Z)
-export const validateNewPassword = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const validateNewPassword = (password) => {
+  const requirements = {
+    lowercase: x => x.toUpperCase() !== x,
+    uppercase: x => x.toLowerCase() !== x,
+    number: x => !isNaN(parseInt(x, 10)),
+    special: x => /[^a-zA-Z0-9]/.test(x),
+  };
+
+  // Length validation
+  if (password.length < 6) {
+    return false;
+  }
+
+  // Char requirements validation
+  for (const char of password) {
+    for (const r in requirements) {
+      if (requirements[r](char)) {
+        delete requirements[r];
+      }
+    }
+  }
+
+  return Object.keys(requirements).length === 0;
 };
