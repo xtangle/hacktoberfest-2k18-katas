f71e0b00d851a33f2f4f4ef57a69e884b8c53ad5
Moritz Schramm <moritz.schramm@sap.com>
2018-10-10 13:50:38 +0200

    Solve twitter block message generator chellange

---
+++ src/isPalindrom.js
@@ -0,0 +1,7 @@
+export const isPalindrom = (word) => {
+  // The function should return true or false if a
+  // word is a palindrom or not
+
+  // A palindrom is a word the means backwards the
+  // same as foreward (e.g. Anna)
+};
