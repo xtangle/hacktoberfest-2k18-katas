dfe99b748799280dc4fb35c5e1f39a14254ea4b8
AliNisarAhmed <ali.nisar87@gmail.com>
2018-10-11 22:01:06 +0500

    solved medianOfTwoSortedArrays.js, added minsweeper.js

--- src/medianOfTwoSortedArrays.js
+++ src/medianOfTwoSortedArrays.js
@@ -1,3 +1,21 @@
 export const medianOfTwoSortedArrays = (arr1, arr2) => {
   // TO IMPLEMENT IN ANOTHER PR
+  let totalLength = arr1.length + arr2.length;
+  let midpoint = Math.floor(totalLength / 2);
+  let arr = [];
+
+  while (arr.length !== midpoint + 1) {
+
+  	if (!arr2.length || arr1[0] <= arr2[0]) {
+  		arr.push(arr1.shift());
+  	} else if (!arr1.length || arr2[0] < arr1[0]) {
+  		arr.push(arr2.shift());
+  	}
+  }
+
+  if (totalLength % 2 !== 0) {
+  	return arr[arr.length - 1];
+  } else {
+  	return (arr[arr.length - 1] + arr[arr.length - 2]) / 2;
+  }
 };
---
+++ src/minsweeper.js
@@ -0,0 +1,9 @@
+export const minsweeper = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+
+  /*  
+
+  In the popular Minesweeper game you have a board with some mines and those cells that don't contain a mine have a number in it that indicates the total number of mines in the neighboring cells. Starting off with some arrangement of mines we want to create a Minesweeper game setup.
+
+  */
+};
