757695274918c0f7cbf80593db23afe8b6d31dd6
WeiChien Hsu <weichien711@gmail.com>
2018-10-11 11:17:08 -0500

    :hammer: Add several test cases for isValidParentheses.test.js

--- src/isValidParentheses.test.js
+++ src/isValidParentheses.test.js
@@ -1,34 +1,23 @@
 import { isValidParentheses } from './isValidParentheses';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(isValidParentheses()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(isValidParentheses()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(isValidParentheses()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(isValidParentheses()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { isValidParentheses(); }).toThrow();
- */
-
 describe('isValidParentheses', () => {
-  it('isValidParentheses does this thing...', () => {
-    // TODO Your own assertion here
+  it('Empty string will be an invalid string.', () => {
+    expect(isValidParentheses("")).toBeFalsy();
   });
 
-  it('isValidParentheses does that other thing...', () => {
-    // TODO Your own assertion here
+  it("a string containing just the characters '(', ')', '{', '}', '[' and ']'", () => {
+    expect(isValidParentheses("abcdefg")).toBeFalsy();
+    expect(isValidParentheses("234123")).toBeFalsy();
   });
 
-  it('isValidParentheses does a very cool thing...', () => {
-    // TODO Your own assertion here
+  it('Handles valid input by returning Ture', () => {
+    expect(isValidParentheses("()")).toBeTruthy();
+    expect(isValidParentheses("{[]}")).toBeTruthy();
+    expect(isValidParentheses("()[]{}")).toBeTruthy();
+  });
+
+  it('Handles invalid input by returning False', () => {
+    expect(isValidParentheses("(]")).toBeFalsy();
+    expect(isValidParentheses("([)]")).toBeFalsy();
   });
 });
