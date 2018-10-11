5e3dacb0168e7dad130e7f8fbfe4501789a15120
WeiChien Hsu <weichien711@gmail.com>
2018-10-11 11:52:58 -0500

    :tada: Generated two isValidAnagram files

---
+++ src/isValidAnagram.js
@@ -0,0 +1,3 @@
+export const isValidAnagram = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
---
+++ src/isValidAnagram.test.js
@@ -0,0 +1,34 @@
+import { isValidAnagram } from './isValidAnagram';
+
+/*
+ * Example of the assertions you can use for your tests:
+ *
+ * - Testing equality between the function's output and a constant:
+ * expect(isValidAnagram()).toEqual(123);
+ *
+ * - Testing that the function returns null:
+ * expect(isValidAnagram()).toBeNull();
+ *
+ * - Testing that the function returns a falsy value (eg. false, 0, "")
+ * expect(isValidAnagram()).toBeFalsy();
+ *
+ * - Testing that the function returns a truthy value (eg. true, 1, "abc")
+ * expect(isValidAnagram()).toBeTruthy();
+ *
+ * - Testing that the function throws
+ * expect(() => { isValidAnagram(); }).toThrow();
+ */
+
+describe('isValidAnagram', () => {
+  it('isValidAnagram does this thing...', () => {
+    // TODO Your own assertion here
+  });
+
+  it('isValidAnagram does that other thing...', () => {
+    // TODO Your own assertion here
+  });
+
+  it('isValidAnagram does a very cool thing...', () => {
+    // TODO Your own assertion here
+  });
+});
