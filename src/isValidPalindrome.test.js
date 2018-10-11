960322f786042ec0b566fe496d51c8437f9133f5
WeiChien Hsu <weichien711@gmail.com>
2018-10-11 12:50:02 -0500

    :tada: Created two files for isValidPalindrome

---
+++ src/isValidPalindrome.test.js
@@ -0,0 +1,34 @@
+import { isValidPalindrome } from './isValidPalindrome';
+
+/*
+ * Example of the assertions you can use for your tests:
+ *
+ * - Testing equality between the function's output and a constant:
+ * expect(isValidPalindrome()).toEqual(123);
+ *
+ * - Testing that the function returns null:
+ * expect(isValidPalindrome()).toBeNull();
+ *
+ * - Testing that the function returns a falsy value (eg. false, 0, "")
+ * expect(isValidPalindrome()).toBeFalsy();
+ *
+ * - Testing that the function returns a truthy value (eg. true, 1, "abc")
+ * expect(isValidPalindrome()).toBeTruthy();
+ *
+ * - Testing that the function throws
+ * expect(() => { isValidPalindrome(); }).toThrow();
+ */
+
+describe('isValidPalindrome', () => {
+  it('isValidPalindrome does this thing...', () => {
+    // TODO Your own assertion here
+  });
+
+  it('isValidPalindrome does that other thing...', () => {
+    // TODO Your own assertion here
+  });
+
+  it('isValidPalindrome does a very cool thing...', () => {
+    // TODO Your own assertion here
+  });
+});
