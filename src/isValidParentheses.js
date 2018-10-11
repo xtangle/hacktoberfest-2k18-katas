42bcbe074ddd91a6eb7eac75d190cef9f8dc3fd7
Krychniak <sc00by13.mk@gmail.com>
2018-10-11 20:29:09 +0200

    598 Implemented isValidParentheses function

--- src/isValidParentheses.js
+++ src/isValidParentheses.js
@@ -1,3 +1,22 @@
-export const isValidParentheses = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const isValidParentheses = (s) => {
+  var tokens = [];
+  if(s == '') return false;
+  for (let token of s.split('')) {
+    switch(token) {
+      case '(':
+        tokens.push(')');
+        break;
+      case '[':
+        tokens.push(']');
+        break;
+      case '{':
+        tokens.push('}');
+        break;
+      default:
+        if(tokens.pop() != token) {
+          return false;
+        }
+    }
+  }
+  return true;
 };
---
+++ src/largestCommonDivisor.js
@@ -0,0 +1,3 @@
+export const largestCommonDivisor = () => {
+    //PUT YOUR IMPLEMENTATION HERE//
+};
