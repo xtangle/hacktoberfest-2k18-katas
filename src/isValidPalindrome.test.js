3274bd600ba619e6b3e8da9c63da13160dece54b
WeiChien Hsu <weichien711@gmail.com>
2018-10-11 12:52:21 -0500

    :construction: Add Several Test Cases in isValidPalindrome

--- src/isValidPalindrome.test.js
+++ src/isValidPalindrome.test.js
@@ -1,34 +1,19 @@
 import { isValidPalindrome } from './isValidPalindrome';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(isValidPalindrome()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(isValidPalindrome()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(isValidPalindrome()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(isValidPalindrome()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { isValidPalindrome(); }).toThrow();
- */
-
 describe('isValidPalindrome', () => {
-  it('isValidPalindrome does this thing...', () => {
-    // TODO Your own assertion here
+  it('Empty string will be an invalid string.', () => {
+    expect(isValidParentheses("")).toBeFalsy();
   });
 
-  it('isValidPalindrome does that other thing...', () => {
-    // TODO Your own assertion here
+  it('Handles valid input by returning Ture', () => {
+    expect(isValidParentheses("a")).toBeTruthy();
+    expect(isValidParentheses("aba")).toBeTruthy();
+    expect(isValidParentheses("abba")).toBeTruthy();
+    expect(isValidParentheses("  abba")).toBeTruthy();
   });
 
-  it('isValidPalindrome does a very cool thing...', () => {
-    // TODO Your own assertion here
+  it('Handles invalid input by returning False', () => {
+    expect(isValidParentheses("abb")).toBeFalsy();
+    expect(isValidParentheses(" abb")).toBeFalsy();
   });
 });
