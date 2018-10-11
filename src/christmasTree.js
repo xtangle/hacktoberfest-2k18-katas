ecd41f1619d62ee44f6cd17152fd5f3d421250fc
Moritz Schramm <moritz.schramm@sap.com>
2018-10-11 11:40:11 +0200

    Change new kata

---
+++ src/christmasTree.js
@@ -0,0 +1,15 @@
+export const christmasTree = (level) => {
+  // Implement a function the return a christmas tree
+  // as a string
+
+  // The level parameter defines how many levels the
+  // tree should contain
+
+  // Example tree with 3 levels
+  //   X
+  //  XXX
+  // XXXXX
+  //   I
+
+  // If no level is provided the function should throw an error
+};
