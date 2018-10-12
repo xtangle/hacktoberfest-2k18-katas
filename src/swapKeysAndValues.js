8c5acf21abd0daec82a2e5eaad693da2b0d03887
Jason Acido <jasonacido@gmail.com>
2018-10-12 17:46:00 +0800

    changed for-in loop to a forEach loop

--- src/swapKeysAndValues.js
+++ src/swapKeysAndValues.js
@@ -1,7 +1,8 @@
 export const swapKeysAndValues = ( object ) => {
     const swappedObject = {};
-    for ( const key in object ) {
+    Object.keys( object ).forEach(( key ) => {
         swappedObject[ object[ key ]] = Number.parseInt( key, 10 ) || key;
-    }
+
+    });
     return swappedObject;
 };
