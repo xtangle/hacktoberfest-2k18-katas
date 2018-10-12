26f50142ff10058639311c474df3cf1e2ce865f2
AliNisarAhmed <ali.nisar87@gmail.com>
2018-10-12 19:34:46 +0500

    fixed

--- src/medianOfTwoSortedArrays.js
+++ src/medianOfTwoSortedArrays.js
@@ -6,16 +6,16 @@ export const medianOfTwoSortedArrays = (arr1, arr2) => {
 
   while (arr.length !== midpoint + 1) {
 
-  	if (!arr2.length || arr1[0] <= arr2[0]) {
-  		arr.push(arr1.shift());
-  	} else if (!arr1.length || arr2[0] < arr1[0]) {
-  		arr.push(arr2.shift());
-  	}
+    if (!arr2.length || arr1[0] <= arr2[0]) {
+      arr.push(arr1.shift());
+    } else if (!arr1.length || arr2[0] < arr1[0]) {
+      arr.push(arr2.shift());
+    }
   }
 
   if (totalLength % 2 !== 0) {
-  	return arr[arr.length - 1];
+    return arr[arr.length - 1];
   } else {
-  	return (arr[arr.length - 1] + arr[arr.length - 2]) / 2;
+    return (arr[arr.length - 1] + arr[arr.length - 2]) / 2;
   }
 };
