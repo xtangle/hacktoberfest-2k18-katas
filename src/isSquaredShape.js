68acec3cb5e33358eb53eaf6d82f2116f80dee23
Julian Martin <julian.martin@russmedia.com>
2018-10-12 18:38:57 +0200

    Implementation of the isSquaredShape

--- src/isSquaredShape.js
+++ src/isSquaredShape.js
@@ -1,3 +1,15 @@
-export const isSquaredShape = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const isSquaredShape = (shape) => {
+
+  if (typeof shape !== 'string' || shape === '') {
+    return false
+  }
+
+  const parsedShape = shape.split('').filter(i => i !== '\n')
+  const width = parsedShape.findIndex(i => i === ' ') - 1
+
+  if (parsedShape.length === (width * width)) {
+    return true
+  }
+
+  return false;
 };
