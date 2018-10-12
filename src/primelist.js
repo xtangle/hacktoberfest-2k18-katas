bb59cc6c31b09afef2e5be9cd293c7ce086381f2
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-10-12 17:36:01 -0400

    Track7Dev | Prime list

---
+++ src/primelist.js
@@ -0,0 +1,21 @@
+/*
+# ERROR HANDLING:
+#   Return "invalid range" for:
+#     *Negative Numbers
+#     *NAN (Not A Number) Data Types
+#     *Start is greater than end
+# 
+# GOAL:
+#   Return the primes numbers from the given range
+#
+# FORMAT:
+#   "2 3 5 7 11 13 17 19 ..."
+#
+*/
+
+export const primelist = (start, end) => {
+  // TO IMPLEMENT IN ANOTHER PR
+
+};
+
+//console.log(primelist(0,777)); //To Test Your Function
