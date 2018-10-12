6d1cf442c18bb94237af463b0dd9133a6093c067
EByrdS <emmanuel_pajaro@hotmail.com>
2018-10-12 22:13:37 +0000

    Solves christmasTree and adds tribonacci

---
+++ src/tribonacci.js
@@ -0,0 +1,14 @@
+export const tribonacci = (position) => {
+  // Implement a function that returns the Nth element of a tribonacci sequence
+
+  // The positions start counting from one (not zero-based numbering)
+  // The first three elements of the sequence are: 1, 1, 1
+
+  // The Nth element is the sum of the last three numbers:
+  // ElementN = Element(N-1) + Element(N-2) + Element(N-3)
+
+  // Example of the sequence: 1, 1, 1, 3, 5, 9, 17, 31 ...
+
+  // If the element is not positive or is not present throw an error
+  // The tests are correct, have fun!
+};
