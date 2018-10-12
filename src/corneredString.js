d02b83e3cc4be84ff8c821b429026442ca5580eb
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-10-12 16:16:37 -0400

    cornered string

--- src/corneredString.js
+++ src/corneredString.js
@@ -1,3 +1,26 @@
-export const corneredString = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+  export const corneredString = (string) => {
+  if(typeof string !== 'string') return "Invalid Data Type"; // CHECK: See if input type is string
+  if(!string) return string; // CHECK: Empty
+
+  string = string.toUpperCase(); // SET TO UPPERCASE
+
+  let results = ''; // Container for the cornered string
+
+  const strArr = string.split(''); // Separate string
+
+  results += strArr.join(' '); // Add the row
+
+  if(string.length < 2) return results; // CHECK: Row is less than 2 chars just return the row
+
+  strArr.shift(); // Remove first char of the string arr
+
+  results += "\n\n"; // Add new line and space to start column
+
+  results += strArr.join("\n\n"); // Add column by joining the string array by adding two new lines per char
+  
+  return results;
 };
+
+console.log(corneredString({type: 'Invalid Data Type'}));
+
+
