b5ab2535f48cbffee6e476ad14a354e644ef5fd6
Kwabena Boohene <okwabse@gmail.com>
2018-10-12 09:52:18 +0000

    Completed isValidAnagram

--- src/isValidAnagram.js
+++ src/isValidAnagram.js
@@ -1,3 +1,11 @@
-export const isValidAnagram = () => {
+export const isValidAnagram = (a, b) => {
   // TO IMPLEMENT IN ANOTHER PR
+  let aLetters = a.split('').sort().join('');
+  let bLetters = b.split('').sort().join('');
+
+  if (aLetters === bLetters) {
+    return true;
+  } else {
+    return false;
+  }
 };
--- src/isValidAnagram.test.js
+++ src/isValidAnagram.test.js
@@ -1,23 +1,6 @@
-import { isValidAnagram } from './isValidAnagram';
-
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(isValidAnagram()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(isValidAnagram()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(isValidAnagram()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(isValidAnagram()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { isValidAnagram(); }).toThrow();
- */
+import {
+  isValidAnagram
+} from './isValidAnagram';
 
 describe('isValidAnagram', () => {
   it('Passed several valid cases.', () => {
