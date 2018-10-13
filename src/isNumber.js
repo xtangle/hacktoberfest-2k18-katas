a75c411e46ec7f119fa53cc1101134995c33f2a0
Delkhaz <Delkhazibrahimi@gmail.com>
2018-10-12 23:21:19 -0500

    implemented isNumber

--- src/isNumber.js
+++ src/isNumber.js
@@ -1,3 +1,6 @@
-export const isNumber = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const isNumber = (num) => {
+	//\d+ matches one or more any digit
+	//-? zero or one negative sign
+	//(\.\d)? matches a decimal followed by any number of digits 
+	return /^-?\d+(\.\d+)?$/.test(num);
 };
