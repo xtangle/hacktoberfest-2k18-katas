39bc5ae17dba22ba3446e0eb3032612924616e64
Chris54721 <chris54721@outlook.com>
2018-10-13 21:14:35 +0200

    Implement isUpperTriangularMatrix

--- src/isUpperTriangularMatrix.js
+++ src/isUpperTriangularMatrix.js
@@ -1,3 +1,11 @@
 export const isUpperTriangularMatrix = matrix => {
-    // implement me
+    if (!Array.isArray(matrix)) return false;
+    for (const r in matrix) {
+        const row = matrix[r];
+        if (!row || row.length !== matrix.length) return false;
+        for (let i = 0; i < r; i++) {
+            if (row[i] !== 0) return false;
+        }
+    }
+    return true;
 };
