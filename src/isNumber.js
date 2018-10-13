29d6f9bd592dc77b084c8d051207b2dd2b05dfb9
Delkhaz <Delkhazibrahimi@gmail.com>
2018-10-12 23:32:56 -0500

    added impl for isNumber

--- src/isNumber.js
+++ src/isNumber.js
@@ -1,6 +1,6 @@
 export const isNumber = (num) => {
 	//\d+ matches one or more any digit
-	//-? zero or one negative sign
+	//-? matches zero or one negative sign
 	//(\.\d)? matches a decimal followed by any number of digits 
 	return /^-?\d+(\.\d+)?$/.test(num);
 };
