8500d7335da575997bc33a514cd8b3f9760eac8f
Jonas-Carlson Schmidt <jonascarlsonschmidt@googlemail.com>
2018-10-13 17:07:42 +0200

    implement stringRepeat #582

---
+++ src/levenshteinDistance.js
@@ -0,0 +1,3 @@
+export const levenshteinDistance = (string, string) => {
+
+};
--- src/stringRepeat.js
+++ src/stringRepeat.js
@@ -1,3 +1,12 @@
-export const stringRepeat = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const stringRepeat = (string, number) => {
+  if (typeof string !== 'string' || Number.isInteger(number) || number < 0) {
+    throw 'type or value not permitted'
+  }
+  result = '';
+
+  for(i = 0; i < number; i++) {
+    result = result + string;
+  }
+
+  return result;
 };
