88394603fd2651b37a4ecbda72b2db61472abdd1
TiffanieISL <tiffanie@isl.co>
2018-10-13 19:43:36 -0400

    fixes linting issue

--- src/calculateTrig.js
+++ src/calculateTrig.js
@@ -1,7 +1,8 @@
 export const calculateTrig = (func, x) => {
-    return (
-      func === 'cos' ?  Math.cos(x) :
-      func === 'sin' ?  Math.sin(x) :
-      func === 'tan' ?  Math.tan(x) :
-      'null');
-  };
+  return (
+    func === 'cos' ?  Math.cos(x) :
+    func === 'sin' ?  Math.sin(x) :
+    func === 'tan' ?  Math.tan(x) :
+    'null'
+  );
+};
