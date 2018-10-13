302ab67b5afbc7349fb23977c71697133bce4f85
Laronk <karpiuk.janek@gmail.com>
2018-10-13 15:35:28 +0200

    Add sortArray functon code

--- src/sortArray.js
+++ src/sortArray.js
@@ -1,3 +1,15 @@
-export const sortArray = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const sortArray = input => {
+  //exception throwing
+  for(var i = 0; i < input.array.length ; i++){
+    if(data !== parseInt(input.array[i], 10)){
+      // data is not an integer - throw exception
+      throw "Invalid input '"+String(input.array[i])+"' at index "+String(i)
+    }
+  }
+  //sorting algorithm
+  if(input.ascending){
+    input.array = input.array.sort((a, b) => a - b)
+  }else{
+    input.array = input.array.sort((a, b) => b - a)
+  }
 };
