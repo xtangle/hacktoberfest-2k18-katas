0ba6a04bc30eedad8d6347a06aa0af7e111c07e7
Josh <mrjoshbgoldstein@gmail.com>
2018-10-13 09:31:45 -0700

    adding getKeyPaths code

--- src/getKeyPaths.js
+++ src/getKeyPaths.js
@@ -1,3 +1,17 @@
-export const getKeyPaths = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+function getPaths(obj) {
+    const acc = [];
+    Object.entries(obj).map(([key, value]) => {
+        if (value === null || typeof value !== 'object') {
+            acc.push(key);
+        } else {
+            const subPaths = getPaths(value);
+            subPaths.map(p => key + '.' + p).forEach(p => acc.push(p));
+        }
+    });
+    return acc;
+}
+
+export const getKeyPaths = (obj) => {
+    if (Array.isArray(obj) || typeof obj !== 'object') throw 'Invalid input';
+    return getPaths(obj);
 };
