e6cbac90b620c4f1a245a5ae2a1bcfea684920f3
Alex Gale <alexgaleiii@gmail.com>
2018-10-13 14:37:29 +0100

    Add calculateTrig

--- src/isPerfectNumber.js
+++ src/isPerfectNumber.js
@@ -1,15 +1,15 @@
 export const isPerfectNumber = (number) => {
   if (number <= 0 || !Number.isInteger(number)) {
-		return false
-	}
+		return false;
+	};
 
-	let final = 0
+	let final = 0;
 
 	for (let i = 1; i < number; i++) {
 		if (number % i == 0) {
-			final += i
-		}
-	}
+			final += i;
+		};
+	};
 
-	return final == number ? true : false
+	return final == number ? true : false;
 };
