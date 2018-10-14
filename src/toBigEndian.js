b44729537a8f237a8542317b75066662f7a95c25
Roman Frolov <roman.frolov@wings.ai>
2018-10-14 17:32:26 +0300

    added challenge

---
+++ src/toBigEndian.js
@@ -0,0 +1,3 @@
+export const toBigEndian = (input) => {
+    // TODO
+};
---
+++ src/toBigEndian.test.js
@@ -0,0 +1,16 @@
+import { toBigEndian } from './formatNumber';
+
+/*
+ * Example of the assertions you can use for your tests:
+ *
+ * - Testing equality between the function's output and a constant:
+ * expect(toBigEndian(1099511892096)).toEqual('00000000 00000000 00000001 00000100 00000000 00000100 00001000 10000100');
+ *
+ */
+
+describe('toBigEndian (32-bit representation)', () => {
+	it('converts integer to big endian', () => {
+		expect(toBigEndian(1025)).toEqual('00000000 00000000 00000100 00000001');
+		expect(toBigEndian(133124)).toEqual('00000000 00000010 00001000 00000100');
+  });
+});
