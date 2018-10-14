e46c4dc0d5ca05dd59b75dfe4fc762b3b4d0ec70
Renan GEHAN <rgehan94@gmail.com>
2018-10-14 11:33:30 +0200

    Update alphabeticShift.test.js

--- src/alphabeticShift.test.js
+++ src/alphabeticShift.test.js
@@ -1,11 +1,20 @@
 import { alphabeticShift } from './alphabeticShift';
 
 describe('Alphabetic Shift Test Cases', () => {
-    it('retuns a shifted string', () => {
-        expect(alphabeticShift('Pseudopseudohypoparathyroidism')).toEqual('Qtfvepqtfvepizqpqbsbuizspjejtn')
-        expect(alphabeticShift('Floccinaucinihilipilification')).toEqual('Gmpddjobvdjojijmjqjmjgjdbujpo')
-        expect(alphabeticShift('Floccinaucinihilipilification')).toEqual('Gmpddjobvdjojijmjqjmjgjdbujpo')
-        expect(alphabeticShift('Antidisestablishmentarianism')).toEqual('Boujejtftubcmjtinfoubsjbojtn')
-        expect(alphabeticShift('supercalifragilisticexpialidocious')).toEqual('tvqfsdbmjgsbhjmjtujdfyqjbmjepdjpvt')
+    it('returns a shifted string', () => {
+        expect(alphabeticShift('Pseudopseudohypoparathyroidism'))
+          .toEqual('Qtfvepqtfvepizqpqbsbuizspjejtn');
+
+        expect(alphabeticShift('Floccinaucinihilipilification'))
+          .toEqual('Gmpddjobvdjojijmjqjmjgjdbujpo');
+
+        expect(alphabeticShift('Floccinaucinihilipilification'))
+          .toEqual('Gmpddjobvdjojijmjqjmjgjdbujpo');
+
+        expect(alphabeticShift('Antidisestablishmentarianism'))
+          .toEqual('Boujejtftubcmjtinfoubsjbojtn');
+
+        expect(alphabeticShift('supercalifragilisticexpialidocious'))
+          .toEqual('tvqfsdbmjgsbhjmjtujdfyqjbmjepdjpvt');
     });
 });
