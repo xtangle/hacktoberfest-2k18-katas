6f2d8e221d04c29695f6a97d44f4b38727ac2922
phuclam.nguyen <phuclam.nguyen@grabtaxi.com>
2018-10-14 16:20:31 +0800

    Implement #715 and add new kata and kata test files

--- src/countMultiplesOfThreeOrSevenOnRange.js
+++ src/countMultiplesOfThreeOrSevenOnRange.js
@@ -1,3 +1,7 @@
-export const countMultiplesOfThreeOrSevenOnRange = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+const count = (n) => {
+    return Math.floor(n/3) + Math.floor(n/7) - Math.floor(n/21);
+};
+
+export const countMultiplesOfThreeOrSevenOnRange = (lo, hi) => {
+  return count(hi) - count(lo) + ((lo % 3 == 0 || lo % 7 == 0) ? 1 : 0);
 };
---
+++ src/lowerBound.js
@@ -0,0 +1,3 @@
+export const lowerBound = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
