8bae0b35b088192114adaaa43777884f7c4dcd50
Levent <LeventDeniz@gmx.de>
2018-10-14 15:49:35 +0200

    Implement isEmpty

--- src/isEmpty.js
+++ src/isEmpty.js
@@ -1,3 +1,13 @@
-export const isEmpty = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+import {isObject} from "./isObject";
+
+export const isEmpty = (value) => {
+    if (isObject(value)){
+        for(let key in value) {
+            if(value.hasOwnProperty(key))
+                return false;
+        }
+        return true;
+    }
+
+    return value == null || value.length === 0;
 };
