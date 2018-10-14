5a542b9541c47332e4429070ea193230a7792fb5
Andreas Reich <andreas@reich.name>
2018-10-14 15:42:25 +0200

    Add test case for wordWrap

---
+++ src/wordWrap.js
@@ -0,0 +1,11 @@
+/*
+ * The wordWrap function is given a string and a number of characters. It
+ * should word wrap the string into lines of the given maximum length and
+ * return those lines as an array of strings. If a single word is too long to
+ * fit into the given line length, simply "give up" and have that word on its
+ * own on a line that's longer than desired.
+ */
+
+export const wordWrap = (input, lineLength) => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
