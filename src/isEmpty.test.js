38478f913a690109176dfaafd5f799a441b6ec16
Levent <LeventDeniz@gmx.de>
2018-10-14 16:50:24 +0200

    Fix isEmpty test formatting

--- src/isEmpty.test.js
+++ src/isEmpty.test.js
@@ -6,28 +6,28 @@ describe("isEmpty", () => {
   });
 
   it("isEmpty returns true for empty arrays", () => {
-    expect(isEmpty([]).toBeTruthy());
+    expect(isEmpty([])).toBeTruthy();
   });
 
   it("isEmpty to return true for empty strings", () => {
-    expect(isEmpty("").toBeTruthy());
+    expect(isEmpty("")).toBeTruthy();
   });
 
   it("isEmpty to return true for null", () => {
-    expect(isEmpty(null).toBeTruthy());
+    expect(isEmpty(null)).toBeTruthy();
   });
 
   it("isEmpty to return true for undefined", () => {
-    expect(isEmpty(undefined).toBeTruthy());
-    expect(isEmpty().toBeTruthy());
+    expect(isEmpty(undefined)).toBeTruthy();
+    expect(isEmpty()).toBeTruthy();
   });
 
   it("isEmpty to return false for literally everything else", () => {
-    expect(isEmpty({ hi: "hi" }).toBeFalsy());
-    expect(isEmpty([1, 2, 3]).toBeFalsy());
-    expect(isEmpty("hello").toBeFalsy());
-    expect(isEmpty(123).toBeFalsy());
-    expect(isEmpty(true).toBeFalsy());
-    expect(isEmpty(false).toBeFalsy());
+    expect(isEmpty({ hi: "hi" })).toBeFalsy();
+    expect(isEmpty([1, 2, 3])).toBeFalsy();
+    expect(isEmpty("hello")).toBeFalsy();
+    expect(isEmpty(123)).toBeFalsy();
+    expect(isEmpty(true)).toBeFalsy();
+    expect(isEmpty(false)).toBeFalsy();
   });
 });
