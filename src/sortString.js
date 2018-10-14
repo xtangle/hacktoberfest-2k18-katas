e061231a91759384672459c9cda2d093522ed8c7
Panagiotis Georgiadis <panagiwtis.geor@gmail.com>
2018-10-14 18:33:58 +0100

    Implemented String Sorting and added String Interpolation

--- src/sortString.js
+++ src/sortString.js
@@ -1,3 +1,7 @@
-export const sortString = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const sortString = (str) => {
+  if(str.length) {
+    return str.split("").sort().reverse().reduce((x, r) => { return r + x }).trim();
+  } else {
+    return "";
+  }
 };
