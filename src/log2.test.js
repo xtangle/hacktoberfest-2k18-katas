688a9d04d28c18531f72ef12b1f3f4805c5db660
phuclam.nguyen <phuclam.nguyen@grabtaxi.com>
2018-10-14 15:29:05 +0800

    Fix a typo

--- src/log2.test.js
+++ src/log2.test.js
@@ -3,7 +3,7 @@ import { log2 } from './log2';
 describe('log2', () => {
   it('returns the index of the most significant bit of positive numbers', () => {
     expect(log2(4)).toEqual(2);
-    expect(logs(15)).toEqual(3);
+    expect(log2(15)).toEqual(3);
   });
 
   it('returns NaN for negative numbers', () => {
