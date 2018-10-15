71f6a56421abdc2368b87d57c2262fd05f2b4a50
Chris Winter <chris-winter@live.de>
2018-10-16 00:12:51 +0200

    more cases
    two more cases

--- src/whitespaceRemoval.test.js
+++ src/whitespaceRemoval.test.js
@@ -1,26 +1,9 @@
 import { whitespaceRemoval } from './whitespaceRemoval';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(whitespaceRemoval()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(whitespaceRemoval()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(whitespaceRemoval()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(whitespaceRemoval()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { whitespaceRemoval(); }).toThrow();
- */
-
 describe('whitespaceRemoval', () => {
   it('removes whitespaces from strings like this', () => {
     expect(whitespaceRemoval('       Hello World!       ').toEqual('Hello World!'))
+    expect(whitespaceRemoval('       Hacktoberfest').toEqual('Hacktoberfest'))
+    expect(whitespaceRemoval('2018       ').toEqual('2018'))
   });
 });
