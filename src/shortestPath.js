e3a56793fa8eb0e3aec4514595096e928c0ad5e7
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2018-10-15 11:11:31 -0500

    refs #710 shortestPath implementation

--- src/shortestPath.js
+++ src/shortestPath.js
@@ -1,5 +1,19 @@
-import { DGNode } from './createDirectedGraph';
-
 export const shortestPath = (graph, from, to) => {
-  // TO IMPLEMENT IN ANOTHER PR
+  // since the input graph is unweighted, a BFS should be equivalent to Dijkstra
+  let queue = [[from, null]];
+  const prnt = {}, path = [];
+  while (queue.length > 0) {
+    const v = queue.shift();
+    prnt[v[0]] = v[1];
+    if (v[0] === to) {
+      for (let v = to; v !== null; v = prnt[v])
+        path.push(v);
+      return path.reverse()
+    }
+    for (let i in graph.edges[v[0]]) {
+      if (!(graph.edges[v[0]][i] in prnt))
+        queue.push([graph.edges[v[0]][i], v[0]]);
+    }
+  }
+  return null;
 };
