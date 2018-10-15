76a3e438468a75e4271e2e5c43ee3b5d43929b57
Frederik Creemers <frederikcreemers@gmail.com>
2018-10-15 21:18:41 +0200

    Implement adler32.

--- src/adler32.js
+++ src/adler32.js
@@ -1,3 +1,13 @@
-export const adler32 = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const adler32 = (s) => {
+    let a = 1;
+    let b = 0;
+    let l = s.length;
+    for(let i=0; i < l; i++){
+        a += s.charCodeAt(i);
+        b += a
+        a %= 65521
+        b %= 65521
+    }
+    
+    return b * 65536 + a
 };
