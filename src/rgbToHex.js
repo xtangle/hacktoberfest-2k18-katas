7b9380b0d8b461a0f404ad03b38ca2d3a0b15608
Chris Winter <chris-winter@live.de>
2018-10-16 00:05:38 +0200

    added solution and new task

--- src/rgbToHex.js
+++ src/rgbToHex.js
@@ -1,3 +1,35 @@
 export const rgbToHex = (rgbArr) => {
-  // TO IMPLEMENT IN ANOTHER PR
+  if ((rgbArr === undefined)
+  || (rgbArr.length === 0)
+  || (rgbArr.includes(null, false) || rgbArr.includes(false, null))
+  || (typeof(rgbArr[0]) === 'string' || typeof(rgbArr[1]) === 'string' || typeof(rgbArr[2]) === 'string'))
+  {
+    throw 'Not possible. Please, use only numeric values.'
+  }
+  
+  let hexPre = hexCalculator(rgbArr[0]);
+  let hexInf = hexCalculator(rgbArr[1]);
+  let hexSuf = hexCalculator(rgbArr[2]);
+
+  let hexAll = hexPre + hexInf + hexSuf;
+  hexAll = minify(hexAll);
+  return '#' + hexAll;  
 };
+
+const hexCalculator = (rgbValue) => {
+  let hex = Number(rgbValue).toString(16);
+  if (hex.length < 2) {
+       hex = '0' + hex;
+  }
+  return hex;
+}
+
+const minify = (hexValue) => {
+  if(hexValue[0] === hexValue[1]
+  && hexValue[2] === hexValue[3]
+  && hexValue[4] === hexValue[5])
+  {
+    return hexValue[0] + hexValue[2] + hexValue[4];
+  }
+  else return hexValue;
+}
