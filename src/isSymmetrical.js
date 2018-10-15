0ad3a2d343cc346db8f91cf1d219e75e00e37a00
Bharath Kumar <kumarbharath718@gmail.com>
2018-10-16 01:29:58 +0530

    implemented isSymmetrical

--- src/isSymmetrical.js
+++ src/isSymmetrical.js
@@ -1,3 +1,5 @@
-export const isSymmetrical = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const isSymmetrical = (num) => {
+	if (num === null) return false;
+	if (Number(num.toString().split("").reverse().join(""))===num) return true;
+	return false;
 };
