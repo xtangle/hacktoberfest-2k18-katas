2be25baead823e1f6f5d4811961ea94d3a95706c
Andreas Reich <andreas@reich.name>
2018-10-15 10:50:28 +0200

    Implement countBy

--- src/countBy.js
+++ src/countBy.js
@@ -1,3 +1,11 @@
 export const countBy = array => {
-  
+    if (!Array.isArray(array)) throw 'Parameter needs to be an array';
+
+    const output = {};
+    for(let value of array) {
+        if (output.hasOwnProperty(value)) output[value]++;
+        else output[value] = 1;
+    }
+
+    return output;
 };
