7ea1c4d4b19aafbe26149e8f6eeac859290b5c17
Zeroji <zzeroji@gmail.com>
2018-10-16 12:19:08 +0200

    Implement lisp #800

--- src/lisp.js
+++ src/lisp.js
@@ -1,3 +1,18 @@
-export const lisp = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const lisp = (expr) => {
+  var funcs = {
+    'add': (a, b) => (a+b),
+    'sub': (a, b) => (a-b),
+    'mul': (a, b) => (a*b),
+    'div': (a, b) => (a/b),
+    'sqrt': Math.sqrt
+  };
+  var parseExpr = /\(([a-z]+)\s+(\d+)(?:\s+(\d+))?\)/g;
+  var match = null;
+  while(match = parseExpr.exec(expr)) {
+    parseExpr.lastIndex = 0;
+    var func = funcs[match[1]];
+    var result = func(+match[2], +match[3]);
+    expr = expr.replace(match[0], ""+result);
+  }
+  return +expr;
 };
