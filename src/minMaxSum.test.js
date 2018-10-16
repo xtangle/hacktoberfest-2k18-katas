c36f238952285ceefbb7fe52c2bcace1b7aa08b6
Andrejs Rinkevičs <rinkevics.andrejs@gmail.com>
2018-10-16 10:46:53 +0300

    #760 Kata tests are invalid, and also not a Mini-Max

--- src/minMaxSum.test.js
+++ src/minMaxSum.test.js
@@ -12,7 +12,11 @@ describe('minMaxSum', () => {
   });
   it('minMaxSum returns the sum of the smallest and largest numbers in an array', () => {
     expect(minMaxSum([4, 3, 2, 1, 6, 4, 8, 7])).toEqual(9);
-    expect(minMaxSum([10, 6, 23, 7, 12, -4, 0, -12])).toEqual(-13);
-    expect(minMaxSum([-1, -5, -20, -6, -9])).toEqual(-12);
+    expect(minMaxSum([10, 6, 23, 7, 12, -4, 0, -12])).toEqual(
+      Math.min(10, 6, 23, 7, 12, -4, 0, -12) + Math.max(10, 6, 23, 7, 12, -4, 0, -12)
+    );
+    expect(minMaxSum([-1, -5, -20, -6, -9])).toEqual(
+      Math.min(-1, -5, -20, -6, -9) + Math.max(-1, -5, -20, -6, -9)
+    );
   });
 });
