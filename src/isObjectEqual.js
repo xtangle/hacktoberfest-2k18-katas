6a14cefc0bf75762f40e8d551a621150617fac89
Fabiana Ofugi <fabiana.ofugi@gmail.com>
2018-10-15 22:12:50 -0300

    implementing isObjectEqual function and adding arraySum

--- src/isObjectEqual.js
+++ src/isObjectEqual.js
@@ -1,3 +1,26 @@
 export const isObjectEqual = (a, b) => {
-	// TO IMPLEMENT IN ANOTHER PR
+	if (a === b)
+		return true;
+
+	if (a.constructor !== b.constructor)
+		return false;
+	
+	for (var p in a) {
+		if (!a.hasOwnProperty(p))
+			continue;
+  
+		if (!b.hasOwnProperty(p))
+			return false;
+		
+		if (a[p] === b[p])
+			continue;
+		
+		if (typeof(a[p]) !== "object")
+			return false;
+
+		if (!isObjectEqual(a[p], b[p]))
+			return false;
+	}
+
+	return true;
 };
