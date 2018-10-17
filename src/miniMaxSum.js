da6d55fe9e2cd3698fc03006286c1d5887a82c4f
shivanggarg1998 <shivanggarg1998@gmail.com>
2018-10-18 01:31:41 +0530

    Implement MiniMaxSum

--- src/miniMaxSum.js
+++ src/miniMaxSum.js
@@ -1,3 +1,14 @@
-export const miniMaxSum = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const miniMaxSum = (arr) => {
+  if (arr.length == 0) {
+    return null;
+  }
+  var max = Math.max(...arr);
+  var min = Math.min(...arr);
+  if (isNaN(max)) {
+    return null;
+  } else {
+    var sum = arr.reduce((a, b) => a + b, 0);
+
+    return [sum - max, sum - min];
+  }
 };
