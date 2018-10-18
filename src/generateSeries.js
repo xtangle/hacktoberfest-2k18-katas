816795b868c6d5489aabb5d71361ef4e671e2ed7
Andrew Pritchard <andrewjpritchard@gmail.com>
2018-10-18 18:41:39 +0800

    Implemented "generateSeries" and tests for "circumcentre"

--- src/generateSeries.js
+++ src/generateSeries.js
@@ -1,3 +1,17 @@
 export const generateSeries = (startValue, length, generationFunction) => {
-  // TO IMPLEMENT IN ANOTHER PR
+    if (!Number.isInteger(startValue) || !Number.isInteger(length)) {
+        throw "Start value and length must be numbers";
+    }
+    if (length < 0) {
+        throw "Length must be grater than or equal to zero";
+    }
+
+    let arr = [];
+    let acc = startValue;
+    for (let i = 0; i < length; i++) {
+        arr.push(acc);
+        acc = generationFunction(acc);
+    }
+
+    return arr;
 };
