db44914f4b49f0e817f1ceddb73e761e3d2c3601
leodevo <leodevogithub@gmail.com>
2018-10-18 17:06:15 +0200

    Implement twoSum; Add arrayToUpperCase

--- src/twoSum.js
+++ src/twoSum.js
@@ -1,3 +1,18 @@
-export const twoSum = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const twoSum = (arr, sum) => {
+  if (Array.isArray(arr)) {
+    for (var i = 0; i < arr.length; i++)
+    {
+      for (var j = i + 1; j < arr.length; j++)
+      {
+        if (arr[i] + arr[j] === sum)
+        {
+          return [i,j]
+        }
+      }
+    }
+    throw new Error ('no match')
+  }
+  else {
+    throw new Error ('not a correct input')
+  }
 };
