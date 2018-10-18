ab0f554f9303e68f6fa8d412d12a900114489c5d
Vito <vito@igenius.net>
2018-10-18 14:51:12 +0200

    added hashtagGen solution and secondMax tests

--- src/hashtagGen.js
+++ src/hashtagGen.js
@@ -1,3 +1,18 @@
-export const hashtagGen = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+const capitalizeFirstLetter = string => {
+  return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+};
+
+export const hashtagGen = phrase => {
+  if (typeof phrase !== "string") {
+    return null;
+  }
+  const trimmedPhrase = phrase.trim();
+  if (trimmedPhrase === "") {
+    return null;
+  }
+  const words = trimmedPhrase.split(" ");
+  const capitalizedWords = words.map(capitalizeFirstLetter);
+  const capitalizedPhrase = capitalizedWords.join("");
+  const hashtag = `#${capitalizedPhrase}`;
+  return hashtag;
 };
