174acbfe1b8b24f61ba6cf7872f899ad083de613
Bedis ElAcheche <bedis.elacheche@photobox.com>
2018-10-18 14:44:35 +0200

    Implement without; Add elementsDeletion

---
+++ src/elementsDeletion.test.js
@@ -0,0 +1,19 @@
+import { elementsDeletion } from './elementsDeletion';
+
+describe('elementsDeletion', () => {
+  it('returns the number of element deletions required', () => {
+    expect(() => elementsDeletion([1, 2, 3, 4], [4, 1, 2])).toEqual(1);
+  });
+
+  it('returns 0 if arrays have the same elements', () => {
+    expect(() => elementsDeletion([1, 2, 3, 4], [4, 3, 1, 2])).toEqual(0);
+  });
+
+  it('works with strings', () => {
+    expect(() => elementsDeletion('bacdc', 'dcbad')).toEqual(2);
+  });
+
+  it('throws error if parameters are missing', () => {
+    expect(() => elementsDeletion()).toThrow();
+  });
+});
