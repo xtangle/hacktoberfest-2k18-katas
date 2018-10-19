0dc831d3ccf6707820b5861ee5d7612d954bd432
kallerdaller <darley0alex1@gmail.com>
2018-10-19 19:24:57 +0100

    Finished number of chars, added camel case

--- src/camelCase.test.js
+++ src/camelCase.test.js
@@ -21,10 +21,10 @@ import { camelCase } from './camelCase';
 
 describe('camelCase', () => {
   it('camelCase remoes all spaces and adds a capital to the beginning of the sentence', () => {
-    expect(numberOfChars("hello world")).toEqual("HelloWorld");
-    expect(numberOfChars("hacktoberfest rocks")).toEqual("HacktoberfestRocks");
-    expect(numberOfChars("this is a large test")).toEqual("ThisIsALargeTest");
-	expect(numberOfChars("one")).toThrowError("Can't convert single word to camel case");
+    expect(camelCase("hello world")).toEqual("HelloWorld");
+    expect(camelCase("hacktoberfest rocks")).toEqual("HacktoberfestRocks");
+    expect(camelCase("this is a large test")).toEqual("ThisIsALargeTest");
+	expect(camelCase("one")).toThrowError("Can't convert single word to camel case");
   });
 });
 
