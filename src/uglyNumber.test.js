28731d20a37580eb28e3c60e293660e2fdef3db8
Gihan Ubayawardana <gubayawardana@LKCOMGUBAYAWA01.local>
2018-10-19 20:48:10 +0530

    implementation and unit tests for ugly numbers

--- src/uglyNumber.test.js
+++ src/uglyNumber.test.js
@@ -1,25 +1,25 @@
-import { uglyNumber } from './uglyNumber';
+import { getNthUglyNumber } from './uglyNumber';
 
 describe('uglyNumber', () => {
 
   it('get 7th uglyNumber', () => {
-    expect(uglyNumber(7)).toEqual(8);
+    expect(getNthUglyNumber(7)).toEqual(8);
   });
 
   it('get 10th uglyNumber', () => {
-    expect(uglyNumber(10)).toEqual(12);
+    expect(getNthUglyNumber(10)).toEqual(12);
   });
 
   it('get 15th uglyNumber', () => {
-    expect(uglyNumber(15)).toEqual(24);
+    expect(getNthUglyNumber(15)).toEqual(24);
   });
 
   it('get 150th uglyNumber', () => {
-    expect(uglyNumber(150)).toEqual(5832);
+    expect(getNthUglyNumber(150)).toEqual(5832);
   });
 
   it('get input not number', () => {
-    expect(() => uglyNumber('-')).toThrow();
+    expect(() => getNthUglyNumber('-')).toThrow();
   });
 
 });
