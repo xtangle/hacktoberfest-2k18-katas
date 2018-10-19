af2aafbe82036a55ad4cbba18dd2e03e1bd31af0
qwewrty <sandeep.carmel@gmail.com>
2018-10-19 23:52:12 +0530

    Update nextPerfectSquare.test.js

--- src/nextPerfectSquare.test.js
+++ src/nextPerfectSquare.test.js
@@ -1,24 +1,5 @@
 import { nextPerfectSquare } from './nextPerfectSquare';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(nextPerfectSquare()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(nextPerfectSquare()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(nextPerfectSquare()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(nextPerfectSquare()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { nextPerfectSquare(); }).toThrow();
- */
-
 describe('nextPerfectSquare', () => {
   it('should return 1 for any negative number', () => {
     expect(nextPerfectSquare(-169)).toEqual(1);
