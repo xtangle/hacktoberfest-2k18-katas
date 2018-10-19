7e45ae8ff0751b9ef07eea287820f9d186db17a8
Jean Benitez <jkbdiaz@gmail.com>
2018-10-19 00:27:14 -0500

    xbonnaci

---
+++ src/countingAnagrams.js
@@ -0,0 +1,3 @@
+export const countingAnagrams = (str) => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
--- src/xbonacci.js
+++ src/xbonacci.js
@@ -1,3 +1,15 @@
 export const xbonacci = (seed, position) => {
-  // TO IMPLEMENT IN ANOTHER PR
+  if (!position || !seed || !Array.isArray(seed) || !seed.length || seed.includes(null)) {
+    throw new Error();
+  }
+
+  const len = seed.length;
+  let currPosition = len;
+  while (currPosition < position) {
+    let sum = seed.slice(seed.length - len).reduce((a, b) => a + b, 0);
+    seed.push(sum);
+    currPosition = seed.length;
+  }
+
+  return seed[position - 1];
 };
