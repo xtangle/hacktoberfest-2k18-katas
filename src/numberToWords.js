c50db43c60fdc79312ca05e80856b5072286fd70
Jean Benitez <jkbdiaz@gmail.com>
2018-10-18 21:55:16 -0500

    Number to words: Refactoring solution and  improving test case

--- src/numberToWords.js
+++ src/numberToWords.js
@@ -4,35 +4,33 @@ export const numberToWords = (number = null) => {
   }
 
   const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const atbts = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eigteen', 'nineteen'];
+  const from10To19 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eigteen', 'nineteen'];
   const dozens = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   const [HUNDRED, AND] = ['hundred', 'and'];
 
-  const arrNumber = String(number).split("");
-  const originalLength = arrNumber.length;
-  let numberInWords = '';
-  let isATBT = false;
+  let numberInWords = [];
 
-  while (arrNumber.length) {
-    const currLength = arrNumber.length;
-    const num = Number(arrNumber.shift());
-    if (currLength === 3) {
-      numberInWords += `${digits[num]} hundred`;
-    } else if (currLength === 2 && num !== 0) {
-      isATBT = num === 1;
-      if (!isATBT) {
-        if (originalLength === 3) {
-          numberInWords += ` and`;
-        }
-        numberInWords += ` ${dozens[num]}`;
-      }
-    } else if (currLength === 1) {
-      if (!isATBT) {
-        numberInWords += ` ${digits[num]}`;
-      } else {
-        numberInWords += ` ${atbts[num]}`;
-      }
+  if (number > 99) {
+    const nHundred = parseInt(number/100);
+    numberInWords.push(digits[nHundred], HUNDRED, AND);
+    number = number - (nHundred * 100);
+  }
+
+  if (number < 20) {
+    if (number > 9) {
+      const lastDigit = number - 10;
+      numberInWords.push(from10To19[lastDigit]);
+    } else {
+      numberInWords.push(digits[number]);
+    }
+  } else {
+    const nDozen = parseInt(number/10);
+    numberInWords.push(dozens[nDozen]);
+    number = number - (nDozen * 10);
+    if (number !== 0) {
+      numberInWords.push(digits[number]);
     }
   }
-  return numberInWords.trim();
+
+  return numberInWords.join(" ").trim();
 };
