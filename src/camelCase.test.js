6d8d2dc85d09831924e755ece86825a3f5a53b08
kallerdaller <darley0alex1@gmail.com>
2018-10-19 19:19:13 +0100

    Finished number of chars, added camel case

--- src/camelCase.test.js
+++ src/camelCase.test.js
@@ -28,24 +28,3 @@ describe('camelCase', () => {
   });
 });
 
-
-
-## Details
-**Issue you fixed:** #879
-
-**Issue you added:** #YYY
-
-**Name of the function you implemented**: numberOfChars
-
-**Name of the function you added test cases for**: camelCase
-
-
-## Checklist (won't be merged if they are not all ticked)
-- [ ✓] I have asked for this issue, and was assigned it
-- [✓ ] I have properly filled the "Details" section above  :arrow_up:
-- [✓ ] I didn't modify any test case  :red_circle:
-- [✓ ] I have implemented the function and all its tests pass  :white_check_mark:
-- [ ✓] I have added a new test case for someone to implement  :new:
-- [ ✓] I have created an issue for my new test case  :speak_no_evil:
-- [✓ ] I've :star: the repository (or not, all up to you)
-- [ ✓✓] And more importantly, I've had fun! :beer:
