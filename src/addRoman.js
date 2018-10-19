2ae5435a817cbb0ac891ddb03006dfcb27ca035d
Jess Mear <jess.mear@endurance.com>
2018-10-19 12:50:27 -0700

    Implement hexToAscii; Add addRoman

---
+++ src/addRoman.js
@@ -0,0 +1,3 @@
+export const addRoman = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
--- src/hexToAscii.js
+++ src/hexToAscii.js
@@ -1,3 +1,11 @@
-export const hexToAscii = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const hexToAscii = (hex) => {
+  let result = hex.split(" ");
+  result = result.map((char) => {
+    char = parseInt(`0x${char}`);
+    if(char > 31 && char < 127) {
+      return String.fromCharCode(char);
+    } 
+  });
+  result = result.join("");
+  return result;
 };
