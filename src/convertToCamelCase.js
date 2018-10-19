0102c8f55597835b28d7d31324e324d0d58b3607
Jean Benitez <jkbdiaz@gmail.com>
2018-10-18 23:38:31 -0500

    Convert to camel case solution and new issue

--- src/convertToCamelCase.js
+++ src/convertToCamelCase.js
@@ -1,3 +1,5 @@
-export const convertToCamelCase = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const convertToCamelCase = (str) => {
+  return typeof str === 'undefined'
+    ? 'Error: No Input String'
+    : str.split(/-|_| /).map(s => s.charAt(0).toUpperCase() + s.substr(1)).join('');
 };
