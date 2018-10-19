c02b59508b00651bfc0d18c43f53a28b475294a7
Emiro Arrieta <emiarrie@gmail.com>
2018-10-19 18:21:37 -0500

    CountMax implementation and pickPeaks (local Maxima) kata

--- src/countMax.js
+++ src/countMax.js
@@ -1,3 +1,15 @@
-export const countMax = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const countMax = (numbers) => {
+  if (!Array.isArray(numbers)) return 0;
+
+  return numbers
+    .reduce((store, item) => {
+      store.counters[item] = (store.counters[item] || 0) + 1;
+      if (item > store.max) store.max = item;
+      store.result = store.counters[store.max];
+      return store;
+    }, {
+      counters: {},
+      max: -Infinity
+    })
+    .result || 0;
 };
---
+++ src/pickPeaks.js
@@ -0,0 +1,3 @@
+export const pickPeaks = (arr) => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
