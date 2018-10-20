6352d8bb1f3677eb3d1f92576fe93dcdf4c23e0c
taverasmisael <macrot29@gmail.com>
2018-10-20 18:58:01 -0400

    add unusedNumbers

---
+++ src/unusedNumbers.test.js
@@ -0,0 +1,20 @@
+import { unusedNumbers } from './unusedNumbers'
+describe('unusedNumbers', () => {
+  it('should return unused digits', () => {
+    expect(unusedNumbers(12, 34, 56, 78)).toBe('09')
+    expect(2015, 8, 26).toBe('3479')
+    expect(276, 575).toBe('013489')
+    expect(643).toBe('0125789')
+    expect(864, 896, 744).toBe('01235')
+    expect(364, 500, 71).toBe('289')
+  })
+
+  it('works with any number of arguments', () => {
+    expect(unusedNumbers()).toBe('0123456789'),
+      expect(unusedNumbers(1, 11, 222, 1111, 44444)).toBe('0356789')
+  })
+
+  it('return an empty string if all numbers were used', () => {
+    expect(unusedNumbers(1, 2, 3, 4, 567, 8, 9, 0, 102, 654)).toBe('')
+  })
+})
