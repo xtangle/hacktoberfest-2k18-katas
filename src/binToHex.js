13097206b4d776797ce6f4bab4527b813c8b0031
Georgy Perepechko <merelj@yandex-team.ru>
2018-10-20 21:12:45 +0300

    binToHex

--- src/binToHex.js
+++ src/binToHex.js
@@ -1,3 +1,5 @@
-export const binToHex = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const binToHex = (bin) => {
+  bin += '';
+  if (!/^(0|1)+$/.test(bin)) { throw new Error ('not a binary number')}
+  return parseInt(bin, 2).toString(16);
 };
---
+++ src/getAccessToArray.js
@@ -0,0 +1,14 @@
+// You need to write a function that takes SafeVector instance as its only argument
+// and returns reference to array inside it; you can mutate the instance.
+
+// function SafeVector(initValues = []) {
+//     const array = initValues; // <-- GET REFERENCE TO THIS!
+//     this.store = (index, value) => array[index] = value;
+//     this.push = (val) => array.push(val);
+//     this.get = (index) => array[index];
+// }
+// const v = new SafeVector()
+
+export const getAccessToArray = (v) => {
+    // return array;
+}
