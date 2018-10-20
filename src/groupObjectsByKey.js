dbb330b37c933064a879739e55f562646ba30500
mithwick93 <mithwick93@gmail.com>
2018-10-20 20:29:06 +0530

    added groupObjectsByKey logic

--- src/groupObjectsByKey.js
+++ src/groupObjectsByKey.js
@@ -1,3 +1,24 @@
-export const groupObjectsByKey = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const groupObjectsByKey = (object, key) => {
+  if (object === undefined || object === null) {
+    throw "Invalid object parameter";
+  }
+
+  if (key === undefined || key === null) {
+    throw "Invalid key parameter";
+  }
+
+  const result = {};
+
+  Object.keys(object).forEach((child) => {
+    const value = object[child];
+    const newKey = value[key];
+
+    if (!result.hasOwnProperty(newKey)) {
+      result[newKey] = {}
+    }
+
+    result[newKey][child] = value;
+  });
+
+  return result;
 };
