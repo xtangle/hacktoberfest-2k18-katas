5679da5c836845b46a66077edfc864b00099d6fd
Plarzoid <plarzoid@gmail.com>
2018-10-20 17:13:50 -0400

    Implement filterObject and Add moneyPrint

--- src/filterObject.js
+++ src/filterObject.js
@@ -1,3 +1,20 @@
-export const filterObject = () => {
-  // TO IMPLEMENT IN ANOTHER PR
-};
+import { isArray } from "util";
+
+export const filterObject = (obj, keys) => {
+  if (typeof obj !== "object" && obj !== null || isArray(obj)) {
+    throw 'First parameter should be an Object'
+  }
+  if (!isArray(keys)) {
+    throw 'Second parameter should be an Array'
+  } else if (keys.length === 0) {
+    throw 'Second Parameter should not be length zero'
+  }
+
+  const result = {}
+  keys.forEach(key => {
+    if (obj[key]) {
+      result[key] = obj[key]
+    }
+  })
+  return result
+}
