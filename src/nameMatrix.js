c849b6297e176d16e64d4652b2222bee1983973a
surmistry <mistrs8@gmail.com>
2018-10-20 17:18:11 -0400

    Implement name matrix

--- src/nameMatrix.js
+++ src/nameMatrix.js
@@ -1,3 +1,23 @@
+const findSquare = (length, n = 1) => {
+  if (n*n >= length) {
+    return n;
+  }
+  return findSquare(length, n+1);
+};
+
+const assembleMatrix = (name, root) => {
+ const matrixCol = [...Array(root)].map((_, i) => i);
+ return matrixCol.map(
+   (_, rowIndex, _arr) =>  matrixCol.map((_, colIndex) => {
+      const indexLength = colIndex + rowIndex * _arr.length;
+      return (indexLength < name.length)
+        ? name[indexLength]
+        : '-';
+   }),
+  );
+}
+
 export const nameMatrix = (name) => {
-  // TO IMPLEMENT IN ANOTHER PR
+  const square = findSquare(name.length);
+  return assembleMatrix(name, square);
 };
