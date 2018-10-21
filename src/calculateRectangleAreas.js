146bcc1ab45c15eb667f906a0614ea5513144a3e
Vivek Kumar Singh <vivek.singh@shipsy.co.in>
2018-10-21 20:10:06 +0530

    fixed Test cases

--- src/calculateRectangleAreas.js
+++ src/calculateRectangleAreas.js
@@ -1,3 +1,10 @@
+
 export const calculateRectangleAreas = (elements) => {
-  return elements.map(element=> element.reduce((a,b) => a*b));
+  let result = []; 	
+  if (Array.isArray(elements) && elements.every(value => Array.isArray(value) && value.length == 2)) {
+  	return elements.map(element=> element.reduce((a,b) => a*b));
+  }
+  else{
+  	return () => {throw `Error at ${elements}`}
+  }		  
 }
