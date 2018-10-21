7c2ce728120ba83a89465def5ba7b02ee1fa7749
Mano3 <33526700+Mano3@users.noreply.github.com>
2018-10-21 18:50:33 +0300

    Update getNextLeapYear.js
    Hope it works.

--- src/getNextLeapYear.js
+++ src/getNextLeapYear.js
@@ -1,3 +1,13 @@
 export const getNextLeapYear = (year) => {
-  // TO IMPLEMENT IN ANOTHER PR
+  var currentYear = (new Date()).getFullYear()+1;
+    while (true) {
+      if(currentYear%4==0) {
+        if(currentYear%100!=0) {
+          return currentYear;
+        }
+        else if (currentYear%400==0) {
+          return currentYear;
+        }
+        currentYear++
+      } 
 };
