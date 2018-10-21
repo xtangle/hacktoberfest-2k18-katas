60abad486a33087fa936cd8a607e5521ff6918f5
Borna Butkovic <bornabutkovic@gmail.com>
2018-10-21 14:29:37 +0200

    Solved sumOfPerfectSquares

--- src/sumOfPerfectSquares.js
+++ src/sumOfPerfectSquares.js
@@ -1,3 +1,15 @@
 export const sumOfPerfectSquares = (n) => {
-  // TO IMPLEMENT IN ANOTHER PR
+    if(n < 4) {
+        return n;
+    }
+    let lowest = n;
+    for(let i = 1; i <= n; i++) {
+        if(Math.pow(i, 2) <= n) {
+            let b = 1 + sumOfPerfectSquares(n - Math.pow(i, 2));
+            lowest = lowest < b ? lowest : b;
+        } else {
+            break;
+        }
+    }
+    return lowest;
 };
