cf9185f9a20396b95b069dc98581bf5ee2ab0286
Marcio Mendes <mmendes.as@gmail.com>
2018-10-21 00:16:32 -0300

    solve positiveAttitude kata #958

--- src/positiveAttitude.js
+++ src/positiveAttitude.js
@@ -1,3 +1,12 @@
-export const positiveAttitude = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const positiveAttitude = (text) => {
+
+  if (!text) {
+    return "Try again!";
+  }
+
+  text = text.replace(/no/ig, "Yes");
+  text = text.replace("can't", "can");
+  text = text.replace("n't", "")
+
+  return text;
 };
