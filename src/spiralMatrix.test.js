bef58c4626e5ad3b7251d109d767b9bf317db5a2
Courtney McCleve <courtney.mccleve@canopytax.com>
2018-10-20 20:14:29 -0600

    Hacktober | Counter clock spiralized matrix

--- src/spiralMatrix.test.js
+++ src/spiralMatrix.test.js
@@ -3,7 +3,9 @@ import { spiralMatrix } from './spiralMatrix';
 describe('spiralMatrix', () => {
   it('Valid test cases', () => {
     expect(spiralMatrix([2])).toEqual([2]);
+  });
 
+  it('should work', () => {
     expect(spiralMatrix(
       [
         [1, 2, 3, 4],
@@ -12,14 +14,18 @@ describe('spiralMatrix', () => {
         [13, 14, 15, 16]
       ]
     )).toEqual([1, 5, 9, 13, 14, 15, 16, 12, 8, 4, 3, 2, 6, 10, 11, 7]);
+  })
 
+  it('spiralizes', () => {
     expect(spiralMatrix(
       [
         [1, 2],
         [3, 4],
       ]
     )).toEqual([1, 3, 4, 2]);
+  })
 
+  it('pleas work', () => {
     expect(spiralMatrix(
       [
         [1, 2, 3, 4, 5, 6],
@@ -27,7 +33,7 @@ describe('spiralMatrix', () => {
         [13, 14, 15, 16, 17, 18],
       ]
     )).toEqual([1, 7, 13, 14, 15, 16, 17, 18, 12, 6, 5, 4, 3, 2, 8, 9, 10, 11]);
-  });
+  })
 
   it('Invalid arg value', () => {
     expect(() => spiralMatrix(null)).toThrow();
