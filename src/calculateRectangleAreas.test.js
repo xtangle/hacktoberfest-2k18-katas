b586d04a77c318605fe5ae1bd99eb8426fda14a0
taverasmisael <macrot29@gmail.com>
2018-10-20 21:02:12 -0400

    add kata to calculate rectangleAreas

---
+++ src/calculateRectangleAreas.test.js
@@ -0,0 +1,17 @@
+import { calculateRectangleAreas } from './calculateRectangleAreas'
+
+describe('calculateRectangleAreas', () => {
+  it('throws if the parameter is not an 2d array', () => {
+    expect(calculateRectangleAreas()).toThrow()
+    expect(calculateRectangleAreas({})).toThrow()
+    expect(calculateRectangleAreas(1)).toThrow()
+    expect(calculateRectangleAreas('asdfasd')).toThrow()
+    expect(calculateRectangleAreas([12, 3])).toThrow()
+  })
+
+  it('calculates all the rectangle areas in a set of 2 dimention array', () => {
+    expect(calculateRectangleAreas([[3, 4]])).toThrow([12])
+    expect(calculateRectangleAreas([[3,4], [8, 10]])).toThrow([12, 80])
+    expect(calculateRectangleAreas([[1,2], [4, 2], [8, 18], [12, 5]])).toThrow([2, 8, 144, 60])
+  })
+})
