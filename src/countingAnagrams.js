9dcfee66925a022e0a83cbf8888d48c0f501ed6e
Nahid Fatma <30729630+nfatma@users.noreply.github.com>
2018-10-21 22:44:41 +0530

    Update countingAnagrams.js

--- src/countingAnagrams.js
+++ src/countingAnagrams.js
@@ -1,40 +1,36 @@
 export const countingAnagrams = (str) => {
   // TO IMPLEMENT IN ANOTHER PR
-  if (typeof str === 'string')
-  {
-      var count = 0;
-      var i = 0;
-      str = str.split(/\s+/);
-      for(var k = 0; k < str.length; k++)
-      {
-          str[k] = str[k].split("");
-          str[k].sort();
-          str[k] = str[k].join("");
-      }
-      while(i < str.length - 1)
-      {
-          var flag = 0;
-          var n1 = str[i].length;
-          for(var j = i + 1; j < str.length; j++)
-          {
-              var n2 = str[j].length;
-              if(n1 == n2 && n1 > 1 && n2 > 1)
-              {
-                      if(str[i] == str[j])
-                      {
-                          if(flag == 0)
-                              count = count + 1;
+};
+export const countingAnagrams = (str) => {
+    var count = 0;
+    var i = 0;
+    str = str.split(/\s+/);
+    for(var k = 0; k < str.length; k++)
+    {
+        str[k] = str[k].split("");
+        str[k].sort();
+        str[k] = str[k].join("");
+    }
+    while(i < str.length - 1)
+    {
+        var flag = 0;
+        var n1 = str[i].length;
+        for(var j = i + 1; j < str.length; j++)
+        {
+            var n2 = str[j].length;
+            if(n1 == n2 && n1 > 1 && n2 > 1)
+            {
+                    if(str[i] == str[j])
+                    {
+                        if(flag == 0)
+                            count = count + 1;
 
-                          flag = flag + 1;
-                          str.splice(j, 1);
-                      }
-              }
-          }
-      i = i + 1;
-      }
-      console.log(count);
-  }
-  else {
-    throw new Error('Enter a Valid String');
-  }
+                        flag = flag + 1;
+                        str.splice(j, 1);
+                    }
+            }
+        }
+    i = i + 1;
+    }
+    return count;
 };
