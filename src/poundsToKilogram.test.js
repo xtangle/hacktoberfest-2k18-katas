467fb2a926f7694af6407cb00b7922f9c44a4624
pckhib <patrickhieber92@gmail.com>
2018-10-21 13:19:26 +0200

    Remove unnecessary lines

--- src/poundsToKilogram.test.js
+++ src/poundsToKilogram.test.js
@@ -1,24 +1,5 @@
 import { poundsToKilogram } from './poundsToKilogram';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(poundsToKilogram()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(poundsToKilogram()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(poundsToKilogram()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(poundsToKilogram()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { poundsToKilogram(); }).toThrow();
- */
-
 describe('poundsToKilogram', () => {
   it('converts pounds to kilogram', () => {
     expect(poundsToKilogram(10)).toEqual(4.54);
@@ -28,14 +9,5 @@ describe('poundsToKilogram', () => {
     expect(poundsToKilogram(10)).not.toEqual(10);
     expect(poundsToKilogram("")).toThrow();
     expect(poundsToKilogram()).toThrow();
-    // TODO Your own assertion here
-  });
-
-  it('poundsToKilogram does that other thing...', () => {
-    // TODO Your own assertion here
-  });
-
-  it('poundsToKilogram does a very cool thing...', () => {
-    // TODO Your own assertion here
   });
 });
