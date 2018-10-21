72b3553e0dba6aaf2f15105aa775e9afd64b612f
Nahid Fatma <30729630+nfatma@users.noreply.github.com>
2018-10-21 22:45:21 +0530

    Update simpleCalculator.test.js

--- src/simpleCalculator.test.js
+++ src/simpleCalculator.test.js
@@ -3,20 +3,20 @@ import { simpleCalculator } from './simpleCalculator';
 
 describe('simpleCalculator', () => {
   it('returns correct result when two integers and a valid operator is passed', () => {
-  except(simpleCalculator(3 4 *).toEqual(12);
+  except(simpleCalculator(3, 4, *).toEqual(12);
   });
 
   it('returns correct result when two integers and a valid operator is passed', () => {
-  except(simpleCalculator(30 14 +).toEqual(44);
+  except(simpleCalculator(30, 14, +).toEqual(44);
   // Valid operands are %, *, /, +, -
   });
 
   it('returns error message when two integers and an invalid operator is passed', () => {
-  except(simpleCalculator(6 2 #).toEqual("invalid Operator");
+  except(simpleCalculator(6, 2, #).toEqual("invalid Operator");
   });
 
   it('returns error message when two integers are not passed', () => {
-  except(simpleCalculator(16 5.9 %).toEqual("invalid or very few operands");
+  except(simpleCalculator(16, 5.9, %).toEqual("invalid or very few operands");
   });
 
 });
