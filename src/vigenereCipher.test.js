604b1ab3b2925431cba92f8d02630e5016001ecc
Andrejs Rinkevičs <rinkevics.andrejs@gmail.com>
2018-10-22 23:44:52 +0300

    Fixed vigenereCipher tests

--- src/vigenereCipher.test.js
+++ src/vigenereCipher.test.js
@@ -2,14 +2,14 @@ import { vigenereCipher } from './vigenereCipher';
 
 describe('vigenereCipher', () => {
   it('vigenere cipher simple test', () => {
-    expect(vigenereCipher('ABC', 'B').toEqual('BCD'))
+    expect(vigenereCipher('ABC', 'B')).toEqual('BCD')
   });
 
   it('vigenere cipher extended test', () => {
-    expect(vigenereCipher('ATTACKATDAWN', 'LEMON').toEqual('LXFOPVEFRNHR'))
+    expect(vigenereCipher('ATTACKATDAWN', 'LEMON')).toEqual('LXFOPVEFRNHR')
   });
 
   it('vigenere cipher rotated', () => {
-    expect(vigenereCipher('HACKTOBERFEST', 'CODE').toEqual('JOFOVCEITTHWV'))
+    expect(vigenereCipher('HACKTOBERFEST', 'CODE')).toEqual('JOFOVCEITTHWV')
   });
 });
