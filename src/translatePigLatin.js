f5d2162c6437459b4f318156b21c424af3070fe7
adamxtokyo <adam.grawender@gmail.com>
2018-10-22 23:34:01 +0200

    Fixes #968

---
+++ src/translatePigLatin.js
@@ -0,0 +1,6 @@
+
+export const translatePigLatin = (str) => {
+  // "When people are taking something extremely seriously,
+  //  that's the time to take out the pig's bladder."
+  //                                         - John Lithgow
+};
